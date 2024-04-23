8329a50300d602dc3af281a578319fb7c95002c6
Kirill Voronin <yoursdearboy@gmail.com>
2024-04-23 09:24:28 +0000

    Added black and formatted files

--- tests/person/test_model.py
+++ tests/person/test_model.py
@@ -4,6 +4,7 @@ from sqlalchemy.orm import Session
 
 from umdb.person.model import Person
 
+
 class TestPersonModel(unittest.TestCase):
     def test_some(self):
         p = Person()
@@ -13,8 +14,8 @@ class TestPersonModel(unittest.TestCase):
         engine = sa.create_engine("sqlite://")
         Person.metadata.create_all(engine)
         with Session(engine) as session:
-            a = Person(id = 1)
+            a = Person(id=1)
             session.add(a)
             session.commit()
-            b = session.query(Person).filter_by(id = 1).one()
+            b = session.query(Person).filter_by(id=1).one()
         self.assertEqual(a.id, b.id)
--- umdb/person/model.py
+++ umdb/person/model.py
@@ -3,13 +3,16 @@ import sqlalchemy as sa
 from sqlalchemy.orm import Mapped, mapped_column
 from sqlalchemy.orm import DeclarativeBase
 
+
 class Base(DeclarativeBase):
     pass
+
 
 class Sex(Enum):
     male = "M"
     female = "F"
     unknown = "U"
+
 
 class Person(Base):
     __tablename__ = "person"
