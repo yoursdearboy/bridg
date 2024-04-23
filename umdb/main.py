ee4269ac31be4774cb2564ad145d4d6ff48d7ee0
Kirill Voronin <yoursdearboy@gmail.com>
2024-04-23 10:05:24 +0000

    Added simple persons index

--- umdb/main.py
+++ umdb/main.py
@@ -1,8 +1,26 @@
-from fastapi import FastAPI
+from fastapi import Depends, FastAPI
+from sqlalchemy.orm import Session
+
+from umdb.db import SessionLocal
+import umdb.person.model as model
+import umdb.person.schema as schema
 
 app = FastAPI()
+
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
 
 
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+
+@app.get("/persons/", response_model=list[schema.Person])
+def list_persons(db: Session = Depends(get_db)):
+    return db.query(model.Person).all()
---
+++ umdb/person/schema.py
@@ -0,0 +1,33 @@
+from datetime import date
+from typing import Optional
+from pydantic import BaseModel
+
+from umdb.person.model import Sex
+
+
+class Name(BaseModel):
+    id: int
+
+    use: Optional[str]
+    family: Optional[str]
+    given: Optional[str]
+    middle: Optional[str]
+    patronymic: Optional[str]
+    prefix: Optional[str]
+    suffix: Optional[str]
+
+    class Config:
+        orm_mode = True
+
+
+class Person(BaseModel):
+    id: int
+    sex: Optional[Sex]
+    birth_date: Optional[date]
+    death_date: Optional[date]
+    death_date_estimated_indicator: Optional[bool]
+    death_indicator: Optional[date]
+    names: list[Name] = []
+
+    class Config:
+        orm_mode = True
