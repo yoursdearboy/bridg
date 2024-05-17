3979607673ad8c9356a00855f4aae9a032cc6e0d
Kirill Voronin <yoursdearboy@gmail.com>
2024-05-17 22:47:30 +0000

    Set up CORS

--- umdb/main.py
+++ umdb/main.py
@@ -1,9 +1,19 @@
 from fastapi import Depends, FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from sqlalchemy.orm import Session
 
 import umdb.person.api
 
 
 app = FastAPI()
+
+# FIXME: move CORS config to a config file
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(umdb.person.api.router, prefix="/persons")
