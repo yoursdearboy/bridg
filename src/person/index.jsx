1bfd15ee45ea301ca8e85350f28c4f657c6ad194
Kirill Voronin <yoursdearboy@gmail.com>
2024-05-18 15:04:10 +0000

    Tweaked data table

--- src/person/index.jsx
+++ src/person/index.jsx
@@ -8,13 +8,31 @@ const URL = "/api/persons";
 
 const COLUMNS = [
   { data: "id", title: "ID" },
+  { data: "primary_name.use", title: "Name usage" },
+  { data: "primary_name.family", title: "Family name" },
+  { data: "primary_name.given", title: "Given name" },
+  { data: "primary_name.middle", title: "Middle name" },
+  { data: "primary_name.patronymic", title: "Patronymic name", visible: false },
+  { data: "primary_name.prefix", title: "Name prefix", visible: false },
+  { data: "primary_name.suffix", title: "Name suffix", visible: false },
   { data: "sex", title: "Sex" },
   {
     data: "birth_date",
     title: "Birth date",
     render: DataTable.render.date("dd.MM.yyyy"),
   },
-  { data: "primary_name.family", title: "Family Name" },
+  {
+    data: "death_date",
+    title: "Death date",
+    render: DataTable.render.date("dd.MM.yyyy"),
+    visible: false,
+  },
+  {
+    data: "death_date_estimated_indicator",
+    title: "Death date estimated?",
+    visible: false,
+  },
+  { data: "death_indicator", title: "Dead" },
 ];
 
 export default function PersonIndex() {
@@ -27,6 +45,9 @@ export default function PersonIndex() {
         dataSrc: "",
       },
       columns: COLUMNS,
+      layout: {
+        topStart: {},
+      },
     });
     return () => dt.destroy();
   });
