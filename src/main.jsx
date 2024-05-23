e4a0fbc9f5e1d0c8cd332684d0ccc1e4bc062ddf
Kirill Voronin <yoursdearboy@gmail.com>
2024-05-23 14:48:41 +0000

    Added Bootstrap dependency

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,6 @@
 import "@fortawesome/fontawesome-free/css/all.css";
 import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap/dist/js/bootstrap.js";
 import "./index.css";
 import React from "react";
 import ReactDOM from "react-dom/client";
