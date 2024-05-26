1f8b550d92f0ca332f09385229549d995f26e789
Kirill Voronin <yoursdearboy@gmail.com>
2024-05-26 11:49:53 +0000

    Themed form labels

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import "@fortawesome/fontawesome-free/css/all.css";
 import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap/dist/js/bootstrap.js";
-import "./index.css";
+import "./main.css";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
