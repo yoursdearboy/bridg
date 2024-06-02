cf4ef158881c0f9c561e2eb62a937667f1b22235
Kirill Voronin <yoursdearboy@gmail.com>
2024-06-02 21:54:25 +0000

    Added Header and Body to Card component

--- src/Card.jsx
+++ src/Card.jsx
@@ -1,7 +1,27 @@
-export default function Card({ children }) {
+import React from "react";
+
+const Header = ({ children, className }) => (
+  <div className={`card-header ${className}`}>{children}</div>
+);
+
+const Body = ({ children, className }) => (
+  <div className={`card-body ${className}`}>{children}</div>
+);
+
+function Card({ children, className }) {
+  const array = React.Children.toArray(children);
+  const header = array.find((child) => child.type == Header);
+  const body = array.find((child) => child.type == Body);
+
   return (
-    <div className="card">
-      <div className="card-body">{children}</div>
+    <div className={`card ${className}`}>
+      {header}
+      {body}
     </div>
   );
 }
+
+Card.Header = Header;
+Card.Body = Body;
+
+export default Card;
