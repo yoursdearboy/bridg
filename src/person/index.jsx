4b232bb4464c5e640a95b9191be475c4c1c3bced
Kirill Voronin <yoursdearboy@gmail.com>
2024-06-03 21:42:59 +0000

    Split index page in two - page and table, modified layout.

--- src/person/index.jsx
+++ src/person/index.jsx
@@ -1,85 +1,28 @@
-import DataTable from "datatables.net-bs5";
-import * as luxon from "luxon";
-import { useEffect, useRef } from "react";
-import { useNavigate } from "react-router-dom";
-import { reactRenderer } from "../datatables";
+import { Link } from "react-router-dom";
+import Breadcrumbs from "../Breadcrumbs";
+import Toolbar from "../Toolbar";
+import PersonTable from "./PersonTable";
 
-window.luxon = luxon;
-
-const URL = "/api/persons";
-
-const COLUMNS = [
-  { data: "id", title: "ID" },
-  { data: "primary_name.use", title: "Name usage" },
-  { data: "primary_name.family", title: "Family name" },
-  { data: "primary_name.given", title: "Given name" },
-  { data: "primary_name.middle", title: "Middle name" },
-  { data: "primary_name.patronymic", title: "Patronymic name", visible: false },
-  { data: "primary_name.prefix", title: "Name prefix", visible: false },
-  { data: "primary_name.suffix", title: "Name suffix", visible: false },
-  { data: "sex", title: "Sex" },
-  {
-    data: "birth_date",
-    title: "Birth date",
-    render: DataTable.render.date("dd.MM.yyyy"),
-  },
-  {
-    data: "death_date",
-    title: "Death date",
-    render: DataTable.render.date("dd.MM.yyyy"),
-    visible: false,
-  },
-  {
-    data: "death_date_estimated_indicator",
-    title: "Death date estimated?",
-    visible: false,
-  },
-  { data: "death_indicator", title: "Dead" },
-  {
-    data: "id",
-    render: reactRenderer(({ data: id, meta }) => {
-      const { navigate } = meta.settings.oInit;
-      const url = `/persons/${id}`;
-      return (
-        <a
-          href={url}
-          onClick={(e) => {
-            e.preventDefault();
-            navigate(url);
-          }}
-        >
-          <i className="fa-solid fa-right-to-bracket"></i>
-        </a>
-      );
-    }),
-    orderable: false,
-  },
-];
-
-export default function PersonIndex() {
-  const ref = useRef();
-  const navigate = useNavigate();
-
-  useEffect(() => {
-    const dt = new DataTable(ref.current, {
-      ajax: {
-        url: URL,
-        dataSrc: "",
-      },
-      columns: COLUMNS,
-      layout: {
-        topStart: {},
-      },
-      searching: false,
-      navigate,
-    });
-    return () => dt.destroy();
-  });
-
+// TODO: Implement layout
+export default function Index() {
   return (
     <div>
-      <h1>Persons</h1>
-      <table className="table" ref={ref}></table>
+      <Breadcrumbs className="mb-1 fs-7">
+        <Breadcrumbs.Item active>Persons</Breadcrumbs.Item>
+      </Breadcrumbs>
+      <Toolbar>
+        <Toolbar.Center>
+          <div className="lead fs-4">List of Persons</div>
+        </Toolbar.Center>
+        <Toolbar.Right>
+          <Link className="btn btn-sm btn-outline-primary" to="new">
+            New
+          </Link>
+        </Toolbar.Right>
+      </Toolbar>
+      <div className="my-3">
+        <PersonTable />
+      </div>
     </div>
   );
 }
