86713e252cfb09bc42b420b4ee7ec5d60e1f1367
Kirill Voronin <yoursdearboy@gmail.com>
2024-06-09 21:59:25 +0000

    Removed web app in React

--- src/Form/Input.tsx
+++
@@ -1,6 +0,0 @@
-import { useFormContext } from "react-hook-form";
-
-export function Input({ name, component: Component, setValueAs, ...props }: any) {
-  const { register } = useFormContext();
-  return <Component {...register(name, { setValueAs })} {...props} />;
-}
