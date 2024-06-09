5efd063bcb01b6397b95db72bfadbff6979aa8d1
Kirill Voronin <yoursdearboy@gmail.com>
2024-06-09 23:30:26 +0000

    Added proxy to API

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,24 @@
 import { fileURLToPath, URL } from 'node:url'
+import process from 'process'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-  ],
+  plugins: [vue()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  },
+  server: {
+    proxy: {
+      '/api': {
+        target: process.env.API_URL,
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, '')
+      }
     }
   }
 })
