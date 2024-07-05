0901e83a70ed942ad97f57baa27f7f7e9141027a
Kirill Voronin <yoursdearboy@gmail.com>
2024-07-05 22:30:26 +0000

    Returned API proxy

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,16 @@
+import process from "process";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      "/api": {
+        target: process.env.API_URL,
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ""),
+      },
+    },
+  },
 });
