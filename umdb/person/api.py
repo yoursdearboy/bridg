fd462244cc9040ad84d5b7b6d6a1baa7d52b7855
Kirill Voronin <yoursdearboy@gmail.com>
2024-07-06 22:33:22 +0000

    Changed API update methods to PATCH

--- umdb/person/api.py
+++ umdb/person/api.py
@@ -33,7 +33,7 @@ def read(id: int, db: orm.Session = Depends(get_db)):
     return person
 
 
-@router.post("/{id}", response_model=schema.Person)
+@router.patch("/{id}", response_model=schema.Person)
 def update(id: int, body: schema.PersonUpdate, db: orm.Session = Depends(get_db)):
     person = db.query(model.Person).where(model.Person.id == id).first()
     if person is None:
