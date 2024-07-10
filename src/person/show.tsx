c6522b10a57855d1061044622dd1c34950f7081a
Kirill Voronin <yoursdearboy@gmail.com>
2024-07-10 16:09:07 +0000

    Reimplemented person show page in Chakra

--- src/person/show.tsx
+++ src/person/show.tsx
@@ -1,9 +1,22 @@
+import {
+  Button,
+  Card,
+  CardBody,
+  CardHeader,
+  Flex,
+  Heading,
+  Menu,
+  MenuButton,
+  MenuItem,
+  MenuList,
+  Spacer,
+  Text,
+  VStack,
+} from "@chakra-ui/react";
 import { Link, useLoaderData, useNavigate } from "react-router-dom";
-import Card from "../components/Card";
-import Toolbar from "../components/Toolbar";
 import * as api from "./api";
 
-const PersonActions = ({ person }) => {
+const PersonMenu = ({ person }) => {
   const name: any = person.primary_name;
 
   const navigate = useNavigate();
@@ -20,47 +33,34 @@ const PersonActions = ({ person }) => {
   };
 
   return (
-    <>
-      <div className="dropdown">
-        <button
-          className="btn btn-primary btn-sm dropdown-toggle"
-          type="button"
-          data-bs-toggle="dropdown"
-        >
-          Actions
-        </button>
-        <ul className="dropdown-menu">
-          <li>
-            <Link className="dropdown-item" to={`name/${name.id}/edit`}>
-              Rename
-            </Link>
-          </li>
-          <li>
-            <button className="dropdown-item" onClick={delete_}>
-              Delete
-            </button>
-          </li>
-        </ul>
-      </div>
-    </>
+    <Menu>
+      <MenuButton as={Button} rightIcon={<i className="fa-solid fa-chevron-down" />}>
+        Actions
+      </MenuButton>
+      <MenuList>
+        <MenuItem as={Link} to={`name/${name.id}/edit`}>
+          Rename
+        </MenuItem>
+        <MenuItem onClick={delete_}>Delete</MenuItem>
+      </MenuList>
+    </Menu>
   );
 };
 
 const PersonCard = ({ person }) => (
-  <Card>
-    <Card.Header className="bg-body">
-      <div className="d-flex">
-        <div className="lead fs-5">Information</div>
-        <div className="ms-auto">
-          <Link to="edit" className="fs-8 align-middle">
-            <i className="fa-solid fa-pencil pe-1" /> Edit
-          </Link>
-        </div>
-      </div>
-    </Card.Header>
-    <Card.Body>
+  <Card maxW="lg">
+    <CardHeader>
+      <Flex>
+        <Heading size="md">Information</Heading>
+        <Spacer />
+        <Link to="edit" className="fs-8 align-middle">
+          <i className="fa-solid fa-pencil pe-1" /> Edit
+        </Link>
+      </Flex>
+    </CardHeader>
+    <CardBody>
       <pre className="mb-0">{JSON.stringify(person, null, 2)}</pre>
-    </Card.Body>
+    </CardBody>
   </Card>
 );
 
@@ -68,23 +68,15 @@ export default function PersonShowPage() {
   const person: any = useLoaderData();
   const name: any = person.primary_name;
   return (
-    <div>
-      <Toolbar>
-        <Toolbar.Center>
-          <div className="lead">
-            <span className="fs-4 me-2">{name?.full}</span>
-            <span className="fs-6">{person.birth_date}</span>
-          </div>
-        </Toolbar.Center>
-        <Toolbar.Right>
-          <PersonActions person={person} />
-        </Toolbar.Right>
-      </Toolbar>
-      <div className="row my-3">
-        <div className="col-sm-4">
-          <PersonCard person={person} />
-        </div>
-      </div>
-    </div>
+    <VStack align="stretch">
+      <Flex>
+        <Text fontSize="2xl" fontWeight="bold">
+          {name?.full}
+        </Text>
+        <Spacer />
+        <PersonMenu person={person} />
+      </Flex>
+      <PersonCard person={person} />
+    </VStack>
   );
 }
