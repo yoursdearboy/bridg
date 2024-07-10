8a52bc8c41b892b0875cd0609714b2de2840e819
Kirill Voronin <yoursdearboy@gmail.com>
2024-07-10 14:41:07 +0000

    Removed bootstrap

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,4 @@
 import "@fortawesome/fontawesome-free/css/all.css";
-import "bootstrap/dist/css/bootstrap.css";
-import "bootstrap/dist/js/bootstrap.js";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
