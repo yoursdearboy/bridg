0fa7187ee9ad4c16bc5c0b17442bea1e50e88906
Kirill Voronin <yoursdearboy@gmail.com>
2024-07-14 07:35:16 +0000

    Created app layout

--- src/AppLayout.vue
+++ src/AppLayout.vue
@@ -3,8 +3,10 @@ import AppNav from './AppNav.vue'
 </script>
 
 <template>
-  <app-nav />
-  <div class="container-xxl">
-    <router-view />
+  <div class="flex flex-col space-y-4">
+    <app-nav />
+    <div class="container mx-auto">
+      <router-view />
+    </div>
   </div>
 </template>
--- src/AppNav.vue
+++ src/AppNav.vue
@@ -1,25 +1,29 @@
+<script setup lang="ts">
+import ThemeController from './ThemeController.vue'
+</script>
+
 <template>
-  <nav class="navbar navbar-expand-lg bg-body-tertiary">
-    <div class="container-xxl">
-      <router-link class="navbar-brand" to="/">umdb</router-link>
-      <button
-        class="navbar-toggler"
-        type="button"
-        data-bs-toggle="collapse"
-        data-bs-target="#nav-content"
-      >
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <div class="collapse navbar-collapse" id="nav-content">
-        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-          <li class="nav-item">
-            <router-link class="nav-link" :to="{ name: 'persons-index' }">Persons</router-link>
+  <div class="navbar bg-base-200 border-b-1 py-0 min-h-10">
+    <div class="container mx-auto">
+      <div>
+        <a class="font-semibold text-lg">
+          <router-link class="navbar-brand" to="/">UMDB</router-link>
+        </a>
+      </div>
+      <div class="w-2"></div>
+      <div>
+        <ul class="menu menu-horizontal">
+          <li>
+            <router-link class="btn btn-sm btn-ghost font-normal" :to="{ name: 'persons-index' }"
+              >Persons</router-link
+            >
           </li>
         </ul>
-        <form class="d-flex" role="search">
-          <input class="form-control me-2" type="search" placeholder="Search" />
-        </form>
+      </div>
+      <div class="flex-1"></div>
+      <div>
+        <theme-controller />
       </div>
     </div>
-  </nav>
+  </div>
 </template>
