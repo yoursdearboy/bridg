7907e22886e2d16e08438e30910a6aa13cf12fdd
Kirill Voronin <kirill.voronin@fccho-moscow.ru>
2024-07-28 22:25:06 +0000

    Refactored app startup

--- web/app.py
+++ web/app.py
@@ -1,10 +1,16 @@
+from dotenv import load_dotenv
 from flask import Flask
 
 from .admin import admin
 from .db import db
+
+load_dotenv()
 
 app = Flask(__name__)
 app.config.from_prefixed_env(prefix="UMDB")
 
 db.init_app(app)
 admin.init_app(app)
+
+if __name__ == "__main__":
+    app.run()
