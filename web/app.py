515d443475163d7a15fe4d9ef65f543cc4233fd0
Kirill Voronin <kirill.voronin@fccho-moscow.ru>
2024-07-30 22:57:51 +0000

    Added Bootstrap

--- web/app.py
+++ web/app.py
@@ -1,5 +1,6 @@
 from dotenv import load_dotenv
 from flask import Flask
+from flask_bootstrap import Bootstrap5
 
 from .admin import admin
 from .db import db
@@ -8,6 +9,8 @@ load_dotenv()
 
 app = Flask(__name__)
 app.config.from_prefixed_env(prefix="UMDB")
+
+bootstrap = Bootstrap5()
 
 db.init_app(app)
 admin.init_app(app)
