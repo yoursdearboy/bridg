e0cb870a683826c4539c3da72aab67636b8b8d04
Kirill Voronin <kirill.voronin@fccho-moscow.ru>
2024-08-21 14:28:27 +0000

    Refactored script to module

--- dev/seed.py
+++ dev/seed.py
@@ -1,82 +1,11 @@
-import logging
-from typing import Any, Dict, List
-
 import yaml
-from sqlalchemy import inspect
-from sqlalchemy.orm import Relationship
 
 from api.db import SessionLocal
-from umdb import (
-    HealthcareFacility,
-    HealthcareProvider,
-    HealthcareProviderGroup,
-    Person,
-    Study,
-)
-
-
-class Cache:
-    def __init__(self):
-        self._values = {}
-
-    def hash(self, key):
-        return id(key)
-
-    def get(self, key):
-        key = self.hash(key)
-        return self._values.get(key)
-
-    def set(self, key, value):
-        key = self.hash(key)
-        self._values[key] = value
-
-
-class Converter:
-    root = dict(
-        person=Person,
-        healthcare_facility=HealthcareFacility,
-        healthcare_provider=HealthcareProvider,
-        healthcare_provider_group=HealthcareProviderGroup,
-        study=Study,
-    )
-
-    def __init__(self):
-        self.cache = Cache()
-
-    def _convert(self, model, value):
-        info = inspect(model)
-        obj = model()
-        for key, value in value.items():
-            attr = info.attrs[key]
-            if isinstance(attr, Relationship):
-                if attr.uselist:
-                    value = [self.convert(attr.entity.class_, v) for v in value]
-                else:
-                    value = self.convert(attr.entity.class_, value)
-            setattr(obj, key, value)
-        return obj
-
-    def convert(self, model, value):
-        if cached := self.cache.get(value):
-            return cached
-        obj = self._convert(model, value)
-        self.cache.set(value, obj)
-        return obj
-
-    def run(self, data: Dict[str, List[Any]]):
-        for key, values in data.items():
-            model = self.root.get(key)
-            if model is None:
-                logging.warn(f"Unknown root model with key {key}")
-                continue
-            for value in values:
-                yield self.convert(model, value)
-
+from umdb.convert import convert
 
 with SessionLocal() as session:
-    with open("demo.yml") as f:
+    with open("dev/seed.yml") as f:
         data = yaml.load(f, yaml.FullLoader)
-    converter = Converter()
-    objects = converter.run(data)
+    objects = convert(data)
     session.add_all(objects)
     session.commit()
---
+++ umdb/convert.py
@@ -0,0 +1,73 @@
+from typing import Any, Dict, List
+
+from sqlalchemy import inspect
+from sqlalchemy.orm import Relationship
+
+from umdb import (
+    HealthcareFacility,
+    HealthcareProvider,
+    HealthcareProviderGroup,
+    Person,
+    Study,
+)
+
+
+class Cache:
+    def __init__(self):
+        self._values = {}
+
+    def hash(self, key):
+        return id(key)
+
+    def get(self, key):
+        key = self.hash(key)
+        return self._values.get(key)
+
+    def set(self, key, value):
+        key = self.hash(key)
+        self._values[key] = value
+
+
+class Converter:
+    root = dict(
+        person=Person,
+        healthcare_facility=HealthcareFacility,
+        healthcare_provider=HealthcareProvider,
+        healthcare_provider_group=HealthcareProviderGroup,
+        study=Study,
+    )
+
+    def __init__(self):
+        self.cache = Cache()
+
+    def _convert(self, model, value):
+        info = inspect(model)
+        obj = model()
+        for key, value in value.items():
+            attr = info.attrs[key]
+            if isinstance(attr, Relationship):
+                if attr.uselist:
+                    value = [self.convert(attr.entity.class_, v) for v in value]
+                else:
+                    value = self.convert(attr.entity.class_, value)
+            setattr(obj, key, value)
+        return obj
+
+    def convert(self, model, value):
+        if cached := self.cache.get(value):
+            return cached
+        obj = self._convert(model, value)
+        self.cache.set(value, obj)
+        return obj
+
+    def run(self, data: Dict[str, List[Any]]):
+        for key, values in data.items():
+            model = self.root.get(key)
+            if model is None:
+                raise RuntimeError(f"Unknown root model with key {key}")
+            for value in values:
+                yield self.convert(model, value)
+
+
+def convert(data: Dict[str, List[Any]]):
+    return Converter().run(data)
