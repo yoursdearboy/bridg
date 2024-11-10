125f879ffa949c421521310259f6f42b12582fd6
Kirill Voronin <kirill.voronin@fccho-moscow.ru>
2024-11-10 20:02:06 +0000

    Made studies page 2 level (Study and StudyProtocolVersion)

--- web/study/__init__.py
+++ web/study/__init__.py
@@ -1,8 +1,10 @@
 from dataclasses import dataclass
+from itertools import groupby
 
 from flask import Blueprint, render_template, request, url_for
 from flask_babel import _
 from sqlalchemy import distinct, func
+from sqlalchemy.orm import Bundle, aliased
 
 from bridg import (
     Study,
@@ -25,46 +27,51 @@ def setup_studies_breadcrumb():
         study_id = request.view_args["study_id"]
         study = db.session.query(Study).filter_by(id=study_id).one_or_none()
         if study:
-            breadcrumbs.append(
-                Breadcrumb(url_for("study.show", id=study_id), str(study))
-            )
-
-
-def count():
-    return (
-        db.session.query(
-            StudyProtocol.planned_study_id.label("study"),
-            func.count(
-                distinct(StudySiteProtocolVersionRelationship.executing_study_site_id)
-            ).label("sites"),
-            func.count(
-                distinct(
-                    StudySubjectProtocolVersionRelationship.assigning_study_subject_id
-                )
-            ).label("subjects"),
-        )
-        .outerjoin(StudyProtocol.versioning_study_protocol_version)
-        .outerjoin(
-            StudyProtocolVersion.executing_study_site_protocol_version_relationship
-        )
-        .outerjoin(
-            StudySiteProtocolVersionRelationship.assigned_study_subject_protocol_version_relationship
-        )
-        .group_by(StudyProtocol.planned_study_id)
-        .all()
-    )
+            breadcrumbs.append(Breadcrumb(url_for("study.show", id=study_id), str(study)))
 
 
 @dataclass
-class StudyInfo:
-    study: Study
+class Count:
+    group: Study | StudyProtocolVersion
     sites: int
     subjects: int
 
 
+def count():
+    base = (
+        db.session.query(
+            func.count(distinct(StudySiteProtocolVersionRelationship.executing_study_site_id)).label("sites"),
+            func.count(distinct(StudySubjectProtocolVersionRelationship.assigning_study_subject_id)).label("subjects"),
+        )
+        .outerjoin(Study.planning_study_protocol)
+        .outerjoin(StudyProtocol.versioning_study_protocol_version)
+        .outerjoin(StudyProtocolVersion.executing_study_site_protocol_version_relationship)
+        .outerjoin(StudySiteProtocolVersionRelationship.assigned_study_subject_protocol_version_relationship)
+    )
+    by_study = base.add_columns(Study).group_by(Study).subquery()
+    by_version = base.add_columns(Study, StudyProtocolVersion).group_by(Study, StudyProtocolVersion).subquery()
+
+    q = db.session.query(
+        aliased(Study, by_study, "study"),
+        Bundle("by_study", by_study.c.sites, by_study.c.subjects),
+        aliased(StudyProtocolVersion, by_version, "version"),
+        Bundle("by_version", by_version.c.sites, by_version.c.subjects),
+    ).outerjoin(by_version, by_study.c.id == by_version.c.id)
+
+    def _study(x):
+        return Count(x.study, **x.by_study._asdict())
+
+    def _version(x):
+        return Count(x.version, **x.by_version._asdict())
+
+    counts = q.all()
+    counts = [(x, list(map(_version, y))) for x, y in groupby(counts, _study)]
+
+    return counts
+
+
 def index():
-    info = [StudyInfo(c.study, c.sites, c.subjects) for c in count()]
-    return render_template("study/index.html", info=info)
+    return render_template("study/index.html", counts=count())
 
 
 blueprint.add_url_rule("/", view_func=index, endpoint="index")
