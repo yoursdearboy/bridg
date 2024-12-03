23798d0e34089edf0325434b61d5bf41e41694c0
anton.sychev <anton.sychev@fccho-moscow.ru>
2024-12-03 08:44:12 +0000

    fix

--- web/tests/test_person.py
+++ web/tests/test_person.py
@@ -30,20 +30,22 @@ def test_person_editname(app, server, page: Page):
     expect(request).to_be_ok()
 
 
-def test_test(app, server, page: Page):
-    url = app.url_for("person.edit", id=7)
+def test_edit(app, server, page: Page):
+    src = {'id': 7, 'administrative_gender_code': AdministrativeGender.male,
+           'death_indicator': False, 'birth_date': datetime.date(1991, 1, 1)}
+    url = app.url_for("person.edit", id=src['id'])
     page.goto(url)
-    page.locator("id=administrative_gender_code").select_option("male")
-    page.locator("id=death_indicator").select_option("false")
-    page.locator("id=birth_date").fill("1991-01-01")
+    page.locator("id=administrative_gender_code").select_option(
+        src['administrative_gender_code'].value)
+    page.locator("id=death_indicator").select_option(
+        str(src['death_indicator']).lower())
+    page.locator("id=birth_date").fill(src['birth_date'].strftime('%Y-%m-%d'))
     page.get_by_text("Save").all()[1].click()
-    data = {'id': 7, 'administrative_gender_code': AdministrativeGender.male,
-            'death_indicator': False, 'birth_date': datetime.date(1991, 1, 1)}
     with app.app_context():
-        result = db.session.query(Person).filter_by(id=7).one()
-    bool = (data['id'] == result.id) & (data['administrative_gender_code'] == result.administrative_gender_code) & (data[
-        'death_indicator'] == result.death_indicator) & (data['birth_date'] == result.birth_date)
-    assert bool
+        result = db.session.query(Person).filter_by(id=src['id']).one()
+        res = {'id': result.id, 'administrative_gender_code': result.administrative_gender_code,
+               'death_indicator': result.death_indicator, 'birth_date': result.birth_date}
+        assert res == src
 
 
 def test_person_editname_save(app, server, page: Page):
@@ -59,6 +61,6 @@ def test_person_editname_save(app, server, page: Page):
     page.get_by_text("Save").all()[1].click()
     with app.app_context():
         result = db.session.query(EntityName).filter_by(id=5).one()
-    res = {'id': result.id, 'family': result.family, 'given': result.given,
-           'middle': result.middle, 'suffix': result.suffix}
-    assert src == res
+        res = {'id': result.id, 'family': result.family, 'given': result.given,
+               'middle': result.middle, 'suffix': result.suffix}
+        assert src == res
