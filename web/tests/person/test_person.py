40e3fe11934f9e8d77e26d8842113908a969c1e5
asychev <anton.sychev@fccho-moscow.ru>
2024-12-17 15:17:33 +0300

    add dissoc

--- web/tests/person/test_person.py
+++ web/tests/person/test_person.py
@@ -4,6 +4,7 @@ from playwright.sync_api import Page, expect
 
 from bridg import AdministrativeGender, Person
 from web.db import db
+from toolz import dissoc
 
 
 def test_person_show(app, server, page: Page):
@@ -20,7 +21,8 @@ def test_person_edit(app, server, page: Page):
 
 def test_edit(app, server, page: Page):
     src = {'id': 7, 'administrative_gender_code': AdministrativeGender.male,
-           'death_indicator': False, 'birth_date': datetime.date(1991, 1, 1)}
+           'death_indicator': False, 'birth_date': datetime.date(1991, 1, 1), 
+           'death_date_estimated_indicator': False, 'type': 'person', 'death_date': None}
     url = app.url_for("person.edit", id=src['id'])
     page.goto(url)
     page.locator("#administrative_gender_code").select_option(
@@ -33,6 +35,5 @@ def test_edit(app, server, page: Page):
     submit.click()
     with app.app_context():
         result = db.session.query(Person).filter_by(id=src['id']).one()
-        res = {'id': result.id, 'administrative_gender_code': result.administrative_gender_code,
-               'death_indicator': result.death_indicator, 'birth_date': result.birth_date}
-        assert res == src
+        res = dissoc(result.__dict__, '_sa_instance_state')
+        assert src == res
