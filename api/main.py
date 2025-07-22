c740e331bfd1bdcff2ba86d5b47d6492b3ccf144
Kirill Voronin <yoursdearboy@gmail.com>
2025-07-22 11:36:46 +0300

    Removed tag on space rotuer

--- api/main.py
+++ api/main.py
@@ -2,7 +2,7 @@ from fastapi import APIRouter, FastAPI
 
 from api import site, subject
 
-space_router = APIRouter(prefix="/space/{space_id:uuid}", tags=["space"])
+space_router = APIRouter(prefix="/space/{space_id:uuid}")
 space_router.include_router(subject.router)
 space_router.include_router(site.router)
 
