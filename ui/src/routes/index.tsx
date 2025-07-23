aab78d7eaf7f9da3d348a02f4a3a02c46ad8a316
Kirill Voronin <yoursdearboy@gmail.com>
2025-07-23 23:59:10 +0300

    Added router

--- ui/src/routes/index.tsx
+++ ui/src/routes/index.tsx
@@ -1,7 +1,12 @@
 import { useQuery } from "@tanstack/react-query";
-import api from "./api";
+import { createFileRoute } from "@tanstack/react-router";
+import api from "../api";
 
-export default function App() {
+export const Route = createFileRoute("/")({
+  component: Index,
+});
+
+function Index() {
   const query = useQuery({
     queryKey: ["spaces"],
     queryFn: () => api.indexSpacesGet(),
