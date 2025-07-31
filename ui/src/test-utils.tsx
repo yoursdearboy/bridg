03d64cc725b4c32274185058939e8dca711199c0
Kirill Voronin <yoursdearboy@gmail.com>
2025-07-31 13:57:23 +0300

    Added snapshot tests

---
+++ ui/src/test-utils.tsx
@@ -0,0 +1,40 @@
+import { MantineProvider } from "@mantine/core";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import {
+  createRootRoute,
+  createRoute,
+  createRouter,
+  RouterProvider,
+  type AnyRoute,
+} from "@tanstack/react-router";
+import { act, render } from "@testing-library/react";
+
+const App = ({ children }: React.PropsWithChildren) => {
+  const queryClient = new QueryClient();
+
+  return (
+    <MantineProvider defaultColorScheme="auto">
+      <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+    </MantineProvider>
+  );
+};
+
+export const renderRoute = (r: AnyRoute) => {
+  const rootRoute = createRootRoute();
+  const testRoute = createRoute({
+    ...r.options,
+    getParentRoute: () => rootRoute,
+    path: "/",
+  });
+  const routeTree = rootRoute.addChildren([testRoute]);
+  const router = createRouter({
+    routeTree,
+  });
+  return act(() =>
+    render(
+      <App>
+        <RouterProvider router={router} />
+      </App>
+    )
+  );
+};
