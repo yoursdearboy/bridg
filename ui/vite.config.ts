bb8188d629ffadbf0a8936728d9ce1b010e0ab72
Kirill Voronin <yoursdearboy@gmail.com>
2025-08-02 00:36:32 +0300

    Fixed routes detection

--- ui/vite.config.ts
+++ ui/vite.config.ts
@@ -17,6 +17,7 @@ export default defineConfig({
     tanstackRouter({
       target: "react",
       autoCodeSplitting: true,
+      routeFileIgnorePattern: ".*\\.test\\.tsx",
     }),
     react(),
   ],
