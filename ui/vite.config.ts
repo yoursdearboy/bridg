fa348264641e91795eb631de0da590ebfdb1a84d
Kirill Voronin <yoursdearboy@gmail.com>
2025-08-12 18:51:59 +0300

    Adjusted vite dev server host addr

--- ui/vite.config.ts
+++ ui/vite.config.ts
@@ -6,6 +6,7 @@ import path from "path";
 // https://vite.dev/config/
 export default defineConfig({
   server: {
+    host: "127.0.0.1",
     proxy: {
       "/api": {
         target: "http://127.0.0.1:8000/",
