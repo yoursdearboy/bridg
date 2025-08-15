b45b98e45b78b0453587c2583f63c47411d53a56
Kirill Voronin <yoursdearboy@gmail.com>
2025-08-15 16:26:10 +0300

    Added type checks to ESLint

--- ui/eslint.config.js
+++ ui/eslint.config.js
@@ -11,13 +11,20 @@ export default tseslint.config([
     files: ["**/*.{ts,tsx}"],
     extends: [
       js.configs.recommended,
-      tseslint.configs.recommended,
+      tseslint.configs.recommendedTypeChecked,
       reactHooks.configs["recommended-latest"],
       reactRefresh.configs.vite,
     ],
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
+      parserOptions: {
+        projectService: true,
+        tsconfigRootDir: import.meta.dirname,
+      },
+    },
+    rules: {
+      "@typescript-eslint/no-unnecessary-condition": "error",
     },
   },
 ]);
