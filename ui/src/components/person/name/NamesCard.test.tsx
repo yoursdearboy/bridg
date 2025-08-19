a3bf0047a5671def9873de7895657789a3735b31
imnotkatya <151066476+imnotkatya@users.noreply.github.com>
2025-08-19 17:28:54 +0300

    snapshot test for namescard

---
+++ ui/src/components/person/name/NamesCard.test.tsx
@@ -0,0 +1,29 @@
+import { expect, it, vi } from "vitest";
+import { renderComponent } from "@/test-utils";
+import { NamesCard } from "./NamesCard";
+import api from "@/api";
+
+it("NamesCard renders correctly", () => {
+  vi.spyOn(api.persons, "indexPersonsPersonIdNamesGet").mockResolvedValue([
+    {
+      family: "Mandy",
+      given: "Gigi",
+      middle: "Gim",
+      patronymic: "—",
+      id: "1",
+    },
+    {
+      family: "Volsi",
+      given: "Wolf",
+      middle: "Fin",
+      patronymic: "—",
+      id: "2",
+    },
+  ]);
+
+  expect(
+    renderComponent(
+      <NamesCard personId="2703d0bc-7ed4-497c-91c6-30d86a8eb630" />
+    ).asFragment()
+  ).toMatchSnapshot();
+});
