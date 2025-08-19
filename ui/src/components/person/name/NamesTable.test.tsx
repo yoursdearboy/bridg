25d7b1283bb187e0ec7873d3d6c6b7ec406ddfca
imnotkatya <k.bydanova@inbox.ru>
2025-08-19 19:31:27 +0300

    Added test for NamesTable

---
+++ ui/src/components/person/name/NamesTable.test.tsx
@@ -0,0 +1,19 @@
+import { describe, it, expect } from "vitest";
+
+import { renderComponent } from "@/test-utils";
+import { NamesTable } from "./NamesTable";
+
+describe("NamesTable", () => {
+  it("matches snapshot", () => {
+    const { asFragment } = renderComponent(
+      <NamesTable
+        names={[
+          { family: "Charlie", given: "Kai", middle: "May", patronymic: "—" },
+          { family: "Nancy", given: "Mia", middle: "Jonson", patronymic: "—" },
+        ]}
+      />
+    );
+
+    expect(asFragment()).toMatchSnapshot();
+  });
+});
