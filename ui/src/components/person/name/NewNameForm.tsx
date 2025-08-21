94a64d9cabbfc55df1b4608d706e203c23c65110
imnotkatya <151066476+imnotkatya@users.noreply.github.com>
2025-08-21 18:11:33 +0300

    split namesform for  edit and new name

---
+++ ui/src/components/person/name/NewNameForm.tsx
@@ -0,0 +1,25 @@
+import { useMutation } from "@tanstack/react-query";
+import api from "@/api";
+import { NameForm } from "./NameForm";
+import type { EntityNameData } from "bridg-ts";
+
+interface Props {
+  personId: string;
+  onClose: () => void;
+  onSuccess: () => void;
+}
+
+export const NewNameForm = ({ personId, onClose, onSuccess }: Props) => {
+  const mutation = useMutation({
+    mutationFn: (data: EntityNameData) =>
+      api.persons.createPersonsPersonIdNamesPost({
+        personId,
+        entityNameData: data,
+      }),
+    onSuccess,
+  });
+
+  return (
+    <NameForm onSubmit={(data) => mutation.mutate(data)} onClose={onClose} />
+  );
+};
