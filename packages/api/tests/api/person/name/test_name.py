e318d78f5dc4967fbd2ee62b2017c3bd6a50e89a
Kirill Voronin <yoursdearboy@gmail.com>
2025-08-21 10:53:06 +0300

    Added factory and implemented test

--- packages/api/tests/api/person/name/test_name.py
+++ packages/api/tests/api/person/name/test_name.py
@@ -1,10 +1,64 @@
+from typing import TypeVar
+
+import bridg
+from api.db import SessionLocal
 from api.main import app
 from fastapi.testclient import TestClient
+from polyfactory import Ignore, Use
+from polyfactory.factories.sqlalchemy_factory import SQLAlchemyFactory
 
 client = TestClient(app)
 
 
+T = TypeVar("T")
+
+
+class BaseFactory(SQLAlchemyFactory[T]):
+    __is_base_factory__ = True
+    __set_relationships__ = False
+    __set_association_proxy__ = False
+    __check_model__ = True
+
+
+class PersonFactory(BaseFactory[bridg.Person]):
+    __set_relationships__ = True
+
+    type = Ignore()
+    # administrative_gender_code
+    death_date = None
+    death_date_estimated_indicator = None
+    death_indicator = False
+    # name
+
+
+class EntityNameFactory(BaseFactory[bridg.EntityName]):
+    __set_as_default_factory_for_type__ = True
+
+    use = None
+    family = Use(BaseFactory.__faker__.last_name)
+    given = Use(BaseFactory.__faker__.first_name)
+    middle = None
+    patronymic = None
+    prefix = None
+    suffix = None
+
+
 def test_answer():
-    person_id = "99132ae7-25dd-43ba-808e-cdb3b29af285"
-    response = client.get(f"/persons/{person_id}/names")
+    PersonFactory.__session__ = SessionLocal()
+    person = PersonFactory.create_sync()
+    response = client.get(f"/persons/{person.id}/names")
     assert response.status_code == 200
+    assert response.json() == [
+        {
+            "id": str(en.id),
+            "label": str(en),
+            "use": None,
+            "family": en.family,
+            "given": en.given,
+            "middle": en.middle,
+            "patronymic": en.patronymic,
+            "prefix": en.prefix,
+            "suffix": en.suffix,
+        }
+        for en in person.name
+    ]
