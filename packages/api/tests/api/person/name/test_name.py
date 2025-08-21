84f5038a70de6ed90536dcf7cc100c503224c6ed
Kirill Voronin <yoursdearboy@gmail.com>
2025-08-21 11:18:20 +0300

    More tests for names

--- packages/api/tests/api/person/name/test_name.py
+++ packages/api/tests/api/person/name/test_name.py
@@ -13,8 +13,11 @@ client = TestClient(app)
 T = TypeVar("T")
 
 
+session = SessionLocal()
+
+
 class BaseFactory(SQLAlchemyFactory[T]):
-    __session__ = SessionLocal()
+    __session__ = session
     __is_base_factory__ = True
     __set_relationships__ = False
     __set_association_proxy__ = False
@@ -24,6 +27,7 @@ class BaseFactory(SQLAlchemyFactory[T]):
 class PersonFactory(BaseFactory[bridg.Person]):
     __set_relationships__ = True
 
+    id = Ignore()
     type = Ignore()
     # administrative_gender_code
     death_date = None
@@ -35,6 +39,7 @@ class PersonFactory(BaseFactory[bridg.Person]):
 class EntityNameFactory(BaseFactory[bridg.EntityName]):
     __set_as_default_factory_for_type__ = True
 
+    id = Ignore()
     use = None
     family = Use(BaseFactory.__faker__.last_name)
     given = Use(BaseFactory.__faker__.first_name)
@@ -44,7 +49,7 @@ class EntityNameFactory(BaseFactory[bridg.EntityName]):
     suffix = None
 
 
-def test_answer():
+def test_person_names():
     person = PersonFactory.create_sync()
     response = client.get(f"/persons/{person.id}/names")
     assert response.status_code == 200
@@ -62,3 +67,71 @@ def test_answer():
         }
         for en in person.name
     ]
+
+
+def test_person_name_create():
+    person = PersonFactory.create_sync(name=[])
+    en = EntityNameFactory.build()
+    data = {
+        "use": None,
+        "family": en.family,
+        "given": en.given,
+        "middle": en.middle,
+        "patronymic": en.patronymic,
+        "prefix": en.prefix,
+        "suffix": en.suffix,
+    }
+    response = client.post(f"/persons/{person.id}/names", json=data)
+    obj = person.primary_name
+    assert obj
+    assert response.status_code == 200
+    assert response.json() == {
+        "id": str(obj.id),
+        "label": str(en),
+        "use": None,
+        "family": en.family,
+        "given": en.given,
+        "middle": en.middle,
+        "patronymic": en.patronymic,
+        "prefix": en.prefix,
+        "suffix": en.suffix,
+    }
+
+
+def test_person_name_update():
+    en = EntityNameFactory.build()
+    en2 = EntityNameFactory.build()
+    person = PersonFactory.create_sync(name=[en])
+    patch = {
+        "use": None,
+        "family": en2.family,
+        "given": en2.given,
+        "middle": en2.middle,
+        "patronymic": en2.patronymic,
+        "prefix": en2.prefix,
+        "suffix": en2.suffix,
+    }
+    response = client.patch(f"/persons/{person.id}/names/{en.id}", json=patch)
+    obj = person.primary_name
+    assert obj
+    assert response.status_code == 200
+    assert response.json() == {
+        "id": str(obj.id),
+        "label": str(en2),
+        "use": None,
+        "family": en2.family,
+        "given": en2.given,
+        "middle": en2.middle,
+        "patronymic": en2.patronymic,
+        "prefix": en2.prefix,
+        "suffix": en2.suffix,
+    }
+
+
+def test_person_name_delete():
+    en = EntityNameFactory.build()
+    person = PersonFactory.create_sync(name=[en])
+    assert len(person.name) == 1
+    client.delete(f"/persons/{person.id}/names/{en.id}")
+    session.refresh(person)
+    assert len(person.name) == 0
