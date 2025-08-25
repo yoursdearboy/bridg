adebfb54cfe866a94682811bbd9b372d47779bd3
imnotkatya <k.bydanova@inbox.ru>
2025-08-25 10:02:55 +0300

    Added translation for nodata

--- ui/src/components/person/name/NameForm.test.tsx
+++ ui/src/components/person/name/NameForm.test.tsx
@@ -1,14 +1,20 @@
 import { describe, it, expect } from "vitest";
 import { NameForm } from "./NameForm";
 import { renderComponent } from "@/test-utils";
+import type { EntityNameData } from "bridg-ts";
 
 describe("NameForm", () => {
   it("matches snapshot", () => {
+    const initialValues: EntityNameData = {
+      family: "Moly",
+      given: "Holy",
+    };
+
     const { asFragment } = renderComponent(
       <NameForm
-        personId="2703d0bc-7ed4-497c-91c6-30d86a8eb630"
+        initialValues={initialValues}
         onClose={() => {}}
-        onSuccess={() => {}}
+        onSubmit={() => {}}
       />
     );
 
