2622d95c55f529ce493dd791f0f6a6a4eb303683
imnotkatya <k.bydanova@inbox.ru>
2025-08-28 10:44:41 +0300

    Fixed buttons layout in NamesTable
    Co-authored-by: Kirill Voronin <yoursdearboy@gmail.com>

--- packages/ui/src/components/person/name/NamesTable.tsx
+++ packages/ui/src/components/person/name/NamesTable.tsx
@@ -1,5 +1,5 @@
 import api from "@/api";
-import { Box, Modal, Table } from "@mantine/core";
+import { Box, Group, Modal, Table } from "@mantine/core";
 import { useDisclosure, useHover } from "@mantine/hooks";
 import { IconPencil, IconX } from "@tabler/icons-react";
 import { useMutation } from "@tanstack/react-query";
@@ -55,12 +55,12 @@ const NamesTableRow = ({ name, personId, onDelete }: NamesTableRowProps) => {
     <>
       <Table.Tr ref={ref}>
         <Table.Td>{name.label}</Table.Td>
-        <Table.Td style={{ width: 80, display: "flex", gap: 8 }}>
+        <Table.Td width={60}>
           {hovered && (
-            <>
+            <Group gap={8}>
               <IconPencil size={16} color="green" onClick={handleEdit} />
               <IconX size={16} color="red" onClick={handleDelete} />
-            </>
+            </Group>
           )}
         </Table.Td>
       </Table.Tr>
