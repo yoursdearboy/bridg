50a29d33179d2d2bf23c1649679c3a0b6a014c36
imnotkatya <151066476+imnotkatya@users.noreply.github.com>
2025-08-28 20:24:45 +0300

    rename_func

--- packages/ui/src/routes/persons/$personId.tsx
+++ packages/ui/src/routes/persons/$personId.tsx
@@ -3,7 +3,16 @@ import ButtonLink from "@/components/ButtonLink";
 import { AddressesCard } from "@/components/person/address/AddressCard";
 import { PersonCard } from "@/components/person/PersonCard";
 import { TelecommunicationAddressesTable } from "@/components/person/TelecommunicationAddressesTable";
-import { Button, Grid, Group, Modal, Space, Stack, Text, Title } from "@mantine/core";
+import {
+  Button,
+  Grid,
+  Group,
+  Modal,
+  Space,
+  Stack,
+  Text,
+  Title,
+} from "@mantine/core";
 import { createFileRoute } from "@tanstack/react-router";
 import type { PersonOutput } from "api-ts";
 import { Route as editRoute } from "./$personId/edit";
@@ -11,6 +20,7 @@ import { useTranslation } from "react-i18next";
 import { NamesCardWrapper } from "@/components/person/name/NamesCard";
 import i18next from "@/i18n";
 import { useDisclosure } from "@mantine/hooks";
+import { EditNameForm } from "@/components/person/name/EditNameForm";
 
 export const Route = createFileRoute("/persons/$personId")({
   component: PersonShowPage,
@@ -23,45 +33,60 @@ export const Route = createFileRoute("/persons/$personId")({
 });
 
 function PersonShowPage() {
-   const [opened, { open, close }] = useDisclosure(false);
+  const [opened, { open, close }] = useDisclosure(false);
   const { personId } = Route.useParams();
   const person = Route.useLoaderData();
-
+  const handleEdit = () => {
+    open();
+  };
   const { t } = useTranslation();
 
   return (
-     <>
-    <Stack gap="md">
-      <Group justify="space-between">
-        <Title fw={500} order={2}>
-          {person.primaryName?.label}
-        </Title>
-     <Group gap="xs" align="flex-end">
-  <ButtonLink to={editRoute.to} params={{ personId }} variant="outline">
-    {t("PersonShowPage.edit")}
-  </ButtonLink>
-  <Button>
-    {t("PersonShowPage.rename")}
-  </Button>
-</Group>
-      </Group>
+    <>
+      <Stack gap="md">
+        <Group justify="space-between">
+          <Title fw={500} order={2}>
+            {person.primaryName?.label}
+          </Title>
+          <Group gap="xs" align="flex-end">
+            <ButtonLink
+              to={editRoute.to}
+              params={{ personId }}
+              variant="outline"
+            >
+              {t("PersonShowPage.edit")}
+            </ButtonLink>
+            <Button onClick={handleEdit}>{t("PersonShowPage.rename")}</Button>
+          </Group>
+        </Group>
 
-      <Grid>
-       
-        <Grid.Col span={{ base: 12, md: 4 }}>
-          <PersonCard person={person} />
-          <Space h="md" />
-          <NamesCardWrapper personId={personId} />
-          <Space h="md" />
-          <AddressesCard personId={personId} />
-          <Space h="md" />
-          <TelecommunicationAddressesTable personId={personId} />
-        </Grid.Col>
-      </Grid>
-    </Stack>
-    <Modal opened={opened} onClose={close} title={t("PersonShowPage.rename")} size="lg">
-          
-          </Modal>
-         </>
+        <Grid>
+          <Grid.Col span={{ base: 12, md: 4 }}>
+            <PersonCard person={person} />
+            <Space h="md" />
+            <NamesCardWrapper personId={personId} />
+            <Space h="md" />
+            <AddressesCard personId={personId} />
+            <Space h="md" />
+            <TelecommunicationAddressesTable personId={personId} />
+          </Grid.Col>
+        </Grid>
+      </Stack>
+      {person.primaryName && (
+        <Modal
+          opened={opened}
+          onClose={close}
+          title={t("PersonShowPage.rename")}
+          size="lg"
+        >
+          <EditNameForm
+            personId={personId}
+            name={person.primaryName}
+            onCancel={close}
+            onSuccess={() => close()}
+          />
+        </Modal>
+      )}
+    </>
   );
 }
