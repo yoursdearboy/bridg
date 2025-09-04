13b70ca81bdf5a99dc9a689dcdc17a4c09f365b3
imnotkatya <k.bydanova@inbox.ru>
2025-09-04 13:42:38 +0300

    Feature/rename (#72)
    * added rename button
    
    * rename_func
    
    * fixed lint
    
    * Added route invalidation on name change
    
    * dropdown used
    
    * fixed lint
    
    ---------
    
    Co-authored-by: Kirill Voronin <yoursdearboy@gmail.com>

--- packages/ui/src/routes/persons/$personId.tsx
+++ packages/ui/src/routes/persons/$personId.tsx
@@ -1,15 +1,25 @@
 import api from "@/api";
-import ButtonLink from "@/components/ButtonLink";
 import { AddressesCard } from "@/components/person/address/AddressCard";
 import { PersonCard } from "@/components/person/PersonCard";
 import { TelecommunicationAddressesTable } from "@/components/person/TelecommunicationAddressesTable";
-import { Grid, Group, Space, Stack, Title } from "@mantine/core";
-import { createFileRoute } from "@tanstack/react-router";
+import {
+  Button,
+  Grid,
+  Group,
+  Menu,
+  Modal,
+  Space,
+  Stack,
+  Title,
+} from "@mantine/core";
+import { createFileRoute, useRouter } from "@tanstack/react-router";
 import type { PersonOutput } from "api-ts";
-import { Route as editRoute } from "./$personId/edit";
 import { useTranslation } from "react-i18next";
 import { NamesCardWrapper } from "@/components/person/name/NamesCard";
 import i18next from "@/i18n";
+import { useDisclosure } from "@mantine/hooks";
+import { EditNameForm } from "@/components/person/name/EditNameForm";
+import { IconChevronDown, IconPencil } from "@tabler/icons-react";
 
 export const Route = createFileRoute("/persons/$personId")({
   component: PersonShowPage,
@@ -22,33 +32,72 @@ export const Route = createFileRoute("/persons/$personId")({
 });
 
 function PersonShowPage() {
+  const [opened, { open, close }] = useDisclosure(false);
   const { personId } = Route.useParams();
   const person = Route.useLoaderData();
-
   const { t } = useTranslation();
+  const router = useRouter();
 
   return (
-    <Stack gap="md">
-      <Group justify="space-between">
-        <Title fw={500} order={2}>
-          {t("PersonShowPage.title")}
-        </Title>
-        <ButtonLink to={editRoute.to} params={{ personId }} variant="outline">
-          {t("PersonShowPage.edit")}
-        </ButtonLink>
-      </Group>
+    <>
+      <Stack gap="md">
+        <Group justify="space-between">
+          <Title fw={500} order={2}>
+            {person.primaryName?.label}
+          </Title>
+          <Group gap="xs" align="flex-end">
+            <Menu shadow="md" width={200} position="bottom-end">
+              <Menu.Target>
+                <Button
+                  variant="outline"
+                  rightSection={<IconChevronDown size={16} />}
+                >
+                  {t("PersonShowPage.actions")}
+                </Button>
+              </Menu.Target>
 
-      <Grid>
-        <Grid.Col span={{ base: 12, md: 4 }}>
-          <PersonCard person={person} />
-          <Space h="md" />
-          <NamesCardWrapper personId={personId} />
-          <Space h="md" />
-          <AddressesCard personId={personId} />
-          <Space h="md" />
-          <TelecommunicationAddressesTable personId={personId} />
-        </Grid.Col>
-      </Grid>
-    </Stack>
+              <Menu.Dropdown>
+                <Menu.Item
+                  leftSection={<IconPencil size={14} />}
+                  onClick={open}
+                >
+                  {t("PersonShowPage.rename")}
+                </Menu.Item>
+              </Menu.Dropdown>
+            </Menu>
+          </Group>
+        </Group>
+
+        <Grid>
+          <Grid.Col span={{ base: 12, md: 4 }}>
+            <PersonCard person={person} />
+            <Space h="md" />
+            <NamesCardWrapper personId={personId} />
+            <Space h="md" />
+            <AddressesCard personId={personId} />
+            <Space h="md" />
+            <TelecommunicationAddressesTable personId={personId} />
+          </Grid.Col>
+        </Grid>
+      </Stack>
+      {person.primaryName && (
+        <Modal
+          opened={opened}
+          onClose={close}
+          title={t("PersonShowPage.rename")}
+          size="lg"
+        >
+          <EditNameForm
+            personId={personId}
+            name={person.primaryName}
+            onCancel={close}
+            onSuccess={() => {
+              void router.invalidate();
+              close();
+            }}
+          />
+        </Modal>
+      )}
+    </>
   );
 }
