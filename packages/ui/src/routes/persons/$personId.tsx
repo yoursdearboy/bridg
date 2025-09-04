6f5cf983a7ddd228c33f04c0a85176e226be303e
Kirill Voronin <yoursdearboy@gmail.com>
2025-09-05 02:44:41 +0300

    Rollback t

--- packages/ui/src/routes/persons/$personId.tsx
+++ packages/ui/src/routes/persons/$personId.tsx
@@ -5,6 +5,7 @@ import { TelecommunicationAddressesTable } from "@/components/person/Telecommuni
 import { Grid, Group, Space, Stack, Title } from "@mantine/core";
 import { createFileRoute } from "@tanstack/react-router";
 import type { ApiPersonPerson } from "api-ts";
+import { useTranslation } from "react-i18next";
 import { NamesCardWrapper } from "@/components/person/name/NamesCard";
 import i18next from "@/i18n";
 
@@ -23,18 +24,19 @@ function PersonShowPage() {
   const { personId } = Route.useParams();
   const person = Route.useLoaderData();
 
+  const { t } = useTranslation();
+
   return (
     <Stack gap="md">
       <Group justify="space-between">
         <Title fw={500} order={2}>
-          {person.primaryName?.label ||
-            i18next.t("PersonShowPage.breadcrumbDefault")}
+          {person.primaryName?.label || t("PersonShowPage.breadcrumbDefault")}
         </Title>
       </Group>
 
       <Grid>
         <Grid.Col span={{ base: 12, md: 4 }}>
-          <PersonCard person={person} personId={personId} />
+          <PersonCard person={person} />
           <Space h="md" />
           <NamesCardWrapper personId={personId} />
           <Space h="md" />
