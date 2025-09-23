5c91a1050ee91b00609220cfd0673e4b52c0b9fb
imnotkatya <151066476+imnotkatya@users.noreply.github.com>
2025-09-23 16:14:48 +0300

    added snapshot tests

---
+++ packages/ui/src/components/person/telecom/TelecommunicationAddressForm.test.tsx
@@ -0,0 +1,56 @@
+import { type UseMutationResult } from "@tanstack/react-query";
+import type {
+  TelecommunicationAddress,
+  TelecommunicationAddressData,
+} from "api-ts";
+import { describe, expect, it, vi } from "vitest";
+import { renderComponent } from "@/test-utils";
+import { TelecommunicationAddressForm } from "./TelecommunicationAddressForm";
+
+describe("TelecommunicationAddressForm", () => {
+  it("matches snapshot", () => {
+    const initialValues: TelecommunicationAddressData = {
+      address: "Pizza",
+      scheme: "ftp",
+      use: "H",
+    };
+    const result = {
+      id: "1",
+      label: "ftp Pizza",
+      ...initialValues,
+    };
+    const mockMutation: UseMutationResult<
+      TelecommunicationAddress,
+      Error,
+      TelecommunicationAddressData,
+      unknown
+    > = {
+      data: result,
+      error: null,
+      isError: false,
+      isIdle: false,
+      isPending: false,
+      isPaused: false,
+      isSuccess: true,
+      failureCount: 0,
+      failureReason: null,
+      mutate: vi.fn(),
+      mutateAsync: vi.fn(),
+      reset: vi.fn(),
+      submittedAt: 0,
+      variables: {},
+      status: "success",
+      context: {},
+    };
+
+    const { asFragment } = renderComponent(
+      <TelecommunicationAddressForm
+        mutation={mockMutation}
+        initialValues={initialValues}
+        onCancel={() => {}}
+      />
+    );
+
+    expect(asFragment()).toMatchSnapshot();
+  });
+});
