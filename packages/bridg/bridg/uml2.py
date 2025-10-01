a35808efc914948066ee76e806467e5f5746767a
Kirill Voronin <yoursdearboy@gmail.com>
2025-10-02 02:33:05 +0300

    Upd

--- packages/bridg/bridg/uml2.py
+++ packages/bridg/bridg/uml2.py
@@ -1,40 +1,91 @@
-import networkx as nx
-import matplotlib.pyplot as plt
-import xml.etree.ElementTree as ET
 import logging
+import xml.etree.ElementTree as ET
+
+import matplotlib.pyplot as plt
+import networkx as nx
 
 MODEL_FILE = "tmp/BRIDG 5.3.1 Comprehensive Domain Information Model.xmi"
 
-G = nx.Graph()
+C = {}
+G = nx.DiGraph()
+A = nx.MultiGraph()
+
+
+def handle_class(event, elem):
+    if "xmi.id" not in elem.attrib or "name" not in elem.attrib:
+        logging.info("No id or name for %s", elem)
+        return
+    id_ = elem.attrib["xmi.id"]
+    name = elem.attrib["name"]
+    C[name] = id_
+    C[id_] = name
+    G.add_node(id_, **elem.attrib)
+    A.add_node(id_, **elem.attrib)
+
+
+def handle_generalization(event, elem):
+    subtype = elem.attrib["subtype"]
+    supertype = elem.attrib["supertype"]
+    G.add_edge(supertype, subtype, **elem.attrib)
+
+
+def handle_association(event, elem):
+    source = elem.find(".//*[@tag='ea_end'][@value='source']/....")
+    target = elem.find(".//*[@tag='ea_end'][@value='target']/....")
+    s = source.attrib["type"]
+    t = target.attrib["type"]
+    for s in nx.dfs_tree(G, s):
+        for t in nx.dfs_tree(G, t):
+            A.add_edge(s, t, source=source.attrib, target=target.attrib)
+
 
 with open(MODEL_FILE, "rb") as f:
+    f.seek(0)
+
     for event, elem in ET.iterparse(f, ("end",)):
         if elem.tag.endswith("Class"):
-            if "xmi.id" not in elem.attrib or "name" not in elem.attrib:
-                logging.error("No id or name for %s", elem)
-                continue
-            id_ = elem.attrib["xmi.id"]
-            G.add_node(id_, **elem.attrib)
-        if elem.tag.endswith("Generalization"):
-            subtype = elem.attrib["subtype"]
-            supertype = elem.attrib["supertype"]
-            G.add_edge(subtype, supertype, **elem.attrib)
+            handle_class(event, elem)
+        elif elem.tag.endswith("Generalization"):
+            handle_generalization(event, elem)
+
+    f.seek(0)
+
+    for event, elem in ET.iterparse(f, ("end",)):
         if elem.tag.endswith("Association"):
-            source = elem.find(".//*[@tag='ea_end'][@value='source']/....")
-            target = elem.find(".//*[@tag='ea_end'][@value='target']/....")
-            G.add_edge(source.attrib["type"], target.attrib["type"])
+            handle_association(event, elem)
 
-def get_class(name):
-    return next(filter(lambda x: x[1]["name"] == name, G.nodes(data=True)))[0]
+PerformedObservation = C["PerformedObservation"]
+PerformedProcedure = C["PerformedProcedure"]
+PerformedObservationResult = C["PerformedObservationResult"]
+Activity = C["Activity"]
 
-PerformedObservation = get_class("PerformedObservation")
-PerformedProcedure = get_class("PerformedProcedure")
-PerformedObservationResult = get_class("PerformedObservationResult")
-paths = nx.all_simple_edge_paths(G, PerformedProcedure, PerformedObservationResult, cutoff=3)
+out = []
+paths = nx.all_simple_edge_paths(A, PerformedProcedure, PerformedObservationResult, cutoff=3)
 for path in paths:
-    print(path)
-#     print(" -> ".join([G.nodes(data=True)[n]["name"] for n in path]))
+    ou = []
+    path_skip = False
+    for edge in path:
+        u, v, key = edge  # type: ignore
+        attrib = A[u][v][key]
+        uattrib = G.nodes[u]
+        vattrib = G.nodes[v]
+        o = f"{uattrib['name']} -- {vattrib['name']} [headlabel={attrib['target']['name']}, taillabel={attrib['source']['name']}]"
+        if (
+            path_skip
+            # or "AdverseEvent" in o
+            # or "SafetyReportVersion" in o
+            # or "PerformedReportGeneration" in o
+            # or "PerformedProductInvestigationResult" in o
+        ):
+            path_skip = True
+            continue
+        if o not in out:
+            ou.append(o)
+    if not path_skip:
+        out += ou
 
-fig = plt.figure()
-nx.draw(G)
-fig.savefig("G.png")
+print("\n".join(out))
+
+# fig = plt.figure()
+# nx.draw(G)
+# fig.savefig("G.png")
