e37f330ee76617870be733fe157a3485df6e5991
Kirill Voronin <yoursdearboy@gmail.com>
2025-10-03 01:50:59 +0300

    Replaced uml to single one

--- packages/bridg/bridg/uml.py
+++ packages/bridg/bridg/uml.py
@@ -3,196 +3,227 @@ FIXME: Split bridg into persistence and this
 """
 
 import logging
+import re
 import xml.etree.ElementTree as ET
-from dataclasses import dataclass
-from functools import reduce
-from io import BufferedReader
-from itertools import groupby
-from typing import Dict, List
+from pathlib import Path
 
-ID = str
-Name = str
+import networkx as nx
 
+MODEL_FILE = "tmp/BRIDG 5.3.1 Comprehensive Domain Information Model.xmi"
 
-@dataclass
-class Attribute:
-    name: Name
+C = {}
+G = nx.DiGraph()
+A = nx.MultiGraph()
 
 
-@dataclass
-class Class:
-    id_: ID
-    name: Name
-    is_abstract: bool
-    attributes: Dict[Name, Attribute]
-
-    def __hash__(self):
-        return hash(self.id_)
-
-
-@dataclass
-class Generalization:
-    name: str
-    subtype: Class
-    supertype: Class
+def handle_class(event, elem):
+    if "xmi.id" not in elem.attrib or "name" not in elem.attrib:
+        logging.info("No id or name for %s", elem)
+        return
+    id_ = elem.attrib["xmi.id"]
+    name = elem.attrib["name"]
+    C[name] = id_
+    C[id_] = name
+    G.add_node(id_, **elem.attrib)
+    A.add_node(id_, **elem.attrib)
 
 
-@dataclass
-class AssociationEnd:
-    name: Name
-    type: Class
+def handle_generalization(event, elem):
+    subtype = elem.attrib["subtype"]
+    supertype = elem.attrib["supertype"]
+    G.add_edge(supertype, subtype, **elem.attrib)
 
 
-@dataclass
-class Association:
-    name: str
-    source: AssociationEnd
-    target: AssociationEnd
+def handle_association(event, elem):
+    source = elem.find(".//*[@tag='ea_end'][@value='source']/....")
+    target = elem.find(".//*[@tag='ea_end'][@value='target']/....")
+    s = source.attrib["type"]
+    t = target.attrib["type"]
+    for s in nx.dfs_tree(G, s):
+        for t in nx.dfs_tree(G, t):
+            A.add_edge(s, t, source=source.attrib, target=target.attrib)
 
 
-@dataclass
-class DB:
-    "Index"
+with open(MODEL_FILE, "rb") as f:
+    f.seek(0)
 
-    classes: Dict[ID | Name, Class]
-    generalizations: List[Generalization]
-    supertypes: Dict[Class, Class]
-    targets: Dict[Class, List[Association]]
+    for event, elem in ET.iterparse(f, ("end",)):
+        if elem.tag.endswith("Class"):
+            handle_class(event, elem)
+        elif elem.tag.endswith("Generalization"):
+            handle_generalization(event, elem)
+
+    f.seek(0)
+
+    for event, elem in ET.iterparse(f, ("end",)):
+        if elem.tag.endswith("Association"):
+            handle_association(event, elem)
+
+PerformedObservation = C["PerformedObservation"]
+PerformedProcedure = C["PerformedProcedure"]
+PerformedObservationResult = C["PerformedObservationResult"]
+Activity = C["Activity"]
+
+SKIP_CLASSES = [
+    r"Adverse*",
+    r"Defined*",
+    r"Molecular*",
+    r"Performed(Exclusion|Inclusion)Criterion",
+    r"PerformedHistopathology*",
+    r"PerformedLesion*",
+    r"PerformedMaterial*",
+    r"PerformedSpecimen*",
+    r"Planned*",
+    r"Scheduled*",
+    "AnatomicPathologySectionVersion",
+    "Biologic",
+    "Biomarker",
+    "CausalAssessment",
+    "CellCulture",
+    "CellLine",
+    "Container",
+    "Cosmetic",
+    "Device",
+    "DocumentVersion",
+    "Drug",
+    "Epoch",
+    "EvaluatedActivityRelationship",
+    "EvaluatedResultRelationship",
+    "ExperimentalActivityItem",
+    "ExperimentalUnit",
+    "FoodProduct",
+    "InterventionalStudyProtocolVersion",
+    "MicrobiologicalCulture",
+    "PerformedDiagnosis",
+    "PerformedEligibilityCriterion",
+    "PerformedExperimentalUnitAllocation",
+    "PerformedGenetic*",
+    "PerformedImagingStudy",
+    "PerformedNotification",
+    "PerformedProductInvestigation",
+    "PerformedProductInvestigationResult",
+    "PerformedProductProblemDiscovery",
+    "PerformedProgressCount",
+    "PerformedProtocolDeviation",
+    "PerformedReportGeneration",
+    "PerformedStudySubjectMilestone",
+    "PerformedSubjectMilestone",
+    "Performer",
+    "Place",
+    "PointOfContact",
+    "Product",
+    "Radiopharmaceutical",
+    "ReportVersion",
+    "ResultClassification",
+    "SafetyReportVersion",
+    "Software",
+    "StandardOfCareDataCollection",
+    "StorageEquipment",
+    "StudyCountry",
+    "StudySite",
+    "TargetAnatomicSite",
+]
+
+# This is action
+SKIP_CLASSES += [
+    "ObservationResultActionTakenRelationship",
+]
+
+# This is action cause
+SKIP_EDGES = [
+    (None, "triggeringPerformedObservationResult"),
+]
 
 
-class Parser:
-    """Parse and index UML file."""
+def _skip_class(id_):
+    name = C[id_]
+    for p in SKIP_CLASSES:
+        if re.match(p, name):
+            return True
 
-    def __init__(self, source: BufferedReader):
-        self.source = source
-        self.namespaces = {}
-        self.classes: Dict[ID | Name, Class] = {}
-        self.generalizations: List[Generalization] = []
-        self.supertypes: Dict[Class, Class] = {}
-        self.associations: List[Association] = []
 
-    def _parse_namespaces(self):
-        for event, elem in ET.iterparse(self.source, events=("start-ns",)):
-            prefix, uri = elem
-            self.namespaces[prefix] = uri
-        self.source.seek(0)
+def _skip_edge(edge):
+    u, v, k = edge
+    uname = C[u]
+    vname = C[v]
+    target = A[u][v][k]["target"]
+    source = A[u][v][k]["source"]
+    tname = target["name"]
+    sname = source["name"]
+    if (uname, tname) in SKIP_EDGES:
+        return True
+    if (None, tname) in SKIP_EDGES:
+        return True
+    if (sname, vname) in SKIP_EDGES:
+        return True
+    if (sname, None) in SKIP_EDGES:
+        return True
+    if (uname, tname, sname, vname) in SKIP_EDGES:
+        return True
+    return False
 
-    def _parse_attribute(self, node: ET.Element):
-        return Attribute(name=node.attrib["name"])
 
-    def _parse_attributes(self, node: ET.Element):
-        nodes = node.findall(".//UML:Attribute", self.namespaces)
-        attributes = [self._parse_attribute(n) for n in nodes]
-        map = {a.name: a for a in attributes}
-        return map
+def _filter_path(path):
+    if any(_skip_class(edge[1]) for edge in path):
+        return False
+    if any(_skip_edge(edge) for edge in path):
+        return False
+    return True
 
-    def _parse_class(self, node: ET.Element):
-        return Class(
-            id_=node.attrib["xmi.id"],
-            name=node.attrib["name"],
-            is_abstract=node.attrib.get("is_abstract", "false") == "true",
-            attributes=self._parse_attributes(node),
+
+out = nx.MultiGraph()
+paths = nx.all_simple_edge_paths(A, PerformedProcedure, PerformedObservationResult, cutoff=3)
+paths = filter(_filter_path, paths)
+for path in paths:
+    for u, v, k in path:  # type: ignore
+        attrib = A[u][v][k]
+        out.add_edge(
+            u,
+            v,
+            k,
+            headlabel=attrib["target"]["name"],
+            taillabel=attrib["source"]["name"],
         )
 
-    def _parse_generalization(self, node: ET.Element):
-        return Generalization(
-            name=node.attrib["name"],
-            subtype=self.classes[node.attrib["subtype"]],
-            supertype=self.classes[node.attrib["supertype"]],
-        )
+print(out)
+for n in out.nodes:
+    print(C[n])
 
-    def _filter_classes(self, nodes: List[ET.Element]):
-        for node in nodes:
-            if node.attrib.get("xmi.id") is None:
-                logging.info("No id for %s", node)
-                continue
-            if node.attrib.get("name") is None:
-                logging.info("No name for %s", node)
-                continue
-            yield node
+out.graph["node"] = {"style": "filled"}
+out.graph["edge"] = {"fontsize": 9}
+out.nodes[PerformedObservation]["fillcolor"] = "lightblue"
+out.nodes[PerformedProcedure]["fillcolor"] = "coral"
+out.nodes[PerformedObservationResult]["fillcolor"] = "lightgreen"
+out.nodes[PerformedProcedure]["pos"] = "-1,-1!"
+out.nodes[PerformedObservation]["pos"] = "2,0!"
+out.nodes[PerformedObservationResult]["pos"] = "2,2!"
 
-    def _parse_classes(self, node: ET.Element):
-        nodes = self._filter_classes(node.findall(".//UML:Class", self.namespaces))
-        classes = [self._parse_class(n) for n in nodes]
-        map = {
-            **{c.id_: c for c in classes},
-            **{c.name: c for c in classes},
-        }
-        return map
+for u, v, k in out.edges(PerformedProcedure, keys=True):
+    out[u][v][k]["color"] = "coral"
 
-    def _parse_generalizations(self, node: ET.Element):
-        nodes = node.findall(".//UML:Generalization", self.namespaces)
-        generalizations = [self._parse_generalization(n) for n in nodes]
-        return generalizations
+for u, v, k in out.edges(PerformedObservation, keys=True):
+    out[u][v][k]["color"] = "lightblue"
 
-    def _parse_supertypes(self):
-        map = {}
-        for g in self.generalizations:
-            if g.subtype in map:
-                raise RuntimeError("Supertype already defined")
-            map[g.subtype] = g.supertype
-        return map
+for u, v, k in out.edges(PerformedObservationResult, keys=True):
+    out[u][v][k]["color"] = "lightgreen"
 
-    def _parse_association(self, node: ET.Element):
-        source = node.find(".//UML:TaggedValue[@tag='ea_end'][@value='source']/....", self.namespaces)
-        target = node.find(".//UML:TaggedValue[@tag='ea_end'][@value='target']/....", self.namespaces)
-        assert source
-        assert target
-        return Association(
-            name=node.attrib["name"],
-            source=AssociationEnd(
-                name=source.attrib["name"],
-                type=self.classes[source.attrib["type"]],
-            ),
-            target=AssociationEnd(
-                name=target.attrib["name"],
-                type=self.classes[target.attrib["type"]],
-            ),
-        )
+out = nx.relabel_nodes(out, C)
 
-    def _parse_associations(self, node: ET.Element):
-        nodes = node.findall(".//UML:Association", self.namespaces)
-        associations = [self._parse_association(n) for n in nodes]
-        return associations
+o = nx.nx_pydot.to_pydot(out)
+o.set_graph_defaults(layout="neato", splines="curved", dpi=300)
+o.set_overlap("scale")
+o.set_sep("+2.0")
+o.write_png("G.png")
 
-    def _parse_targets(self):
-        return {k: list(v) for k, v in groupby(self.associations, lambda a: a.source.type)}
-
-    def _parse_db(self, node: ET.Element):
-        self.classes = self._parse_classes(node)
-        self.generalizations = self._parse_generalizations(node)
-        self.associations = self._parse_associations(node)
-        self.supertypes = self._parse_supertypes()
-        self.targets = self._parse_targets()
-
-    def parse(self):
-        self._parse_namespaces()
-        tree = ET.parse(self.source)
-        root = tree.getroot()
-        self._parse_db(root)
-        return DB(
-            classes=self.classes,
-            generalizations=self.generalizations,
-            supertypes=self.supertypes,
-            targets=self.targets,
-        )
-
-
-def find_supertypes(db, class_: Class):
-    if parent := db.supertypes.get(class_):
-        yield parent
-        find_supertypes(db, parent)
-
-
-def find_attributes_recursively(db, class_):
-    types = [class_] + list(find_supertypes(db, class_))
-    return reduce(lambda a, b: {**a, **b.attributes}, types, {})
-
-
-def find_targets(db, class_):
-    return db.targets[class_]
-
-
-def find_targets_recursively(db, class_):
-    types = [class_] + list(find_supertypes(db, class_))
-    return reduce(lambda a, b: [*a, *find_targets(db, b)], types, [])
+for i, path in enumerate(nx.all_simple_edge_paths(out, "PerformedProcedure", "PerformedObservationResult", cutoff=3)):
+    filepath = []
+    o = nx.Graph()
+    for u, v, k in path:  # type: ignore
+        attr = out[u][v][k]
+        filepath += [attr["headlabel"], attr["taillabel"], v]
+        o.add_edge(u, v, key=k, **attr)
+    o = nx.nx_pydot.to_pydot(o)
+    filename = filepath.pop()
+    filepath = "/".join(["G"] + filepath)
+    Path(filepath).mkdir(parents=True, exist_ok=True)
+    o.write_png(f"{filepath}/{filename}.png")
