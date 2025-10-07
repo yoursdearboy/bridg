04affa30f500dd09674c32e2f986794ed1487d81
Kirill Voronin <yoursdearboy@gmail.com>
2025-10-07 17:29:11 +0300

    Typed icons

--- packages/ui/src/components/person/telecom/icons.ts
+++ packages/ui/src/components/person/telecom/icons.ts
@@ -1,8 +1,21 @@
-import { IconMail, IconPhone, IconWorldWww } from "@tabler/icons-react";
+import {
+  IconMail,
+  IconPhone,
+  IconWorldWww,
+  type Icon,
+  type IconProps,
+} from "@tabler/icons-react";
+import type { URLScheme } from "api-ts";
+import type { ForwardRefExoticComponent, RefAttributes } from "react";
 
-export default {
+const icons: Record<
+  URLScheme,
+  ForwardRefExoticComponent<IconProps & RefAttributes<Icon>>
+> = {
   tel: IconPhone,
   mailto: IconMail,
   ftp: IconWorldWww,
   http: IconWorldWww,
 };
+
+export default icons;
