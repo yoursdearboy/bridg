c4483c6e0e3754dff56b3670bd2d8b0e2643c5e6
Kirill Voronin <yoursdearboy@gmail.com>
2025-10-12 17:59:53 +0300

    format

--- packages/ui/src/components/activity/ActivityForm.tsx
+++ packages/ui/src/components/activity/ActivityForm.tsx
@@ -1,4 +1,4 @@
-import { Group, LoadingOverlay, Stack, Text } from "@mantine/core";
+import { LoadingOverlay, Stack, Text } from "@mantine/core";
 import { useQuery, type UseQueryResult } from "@tanstack/react-query";
 import type { DefinedObservationResult, StudyActivity } from "api-ts";
 import { useTranslation } from "react-i18next";
@@ -34,21 +34,23 @@ interface ActivityFormProps {
   query: UseQueryResult<DefinedObservationResult[], Error>;
 }
 
-const ActivityForm = ({ spaceId, subjectId, query }: ActivityFormProps) => {
+const ActivityForm = ({ query }: ActivityFormProps) => {
   const { t } = useTranslation();
   const { isPending, isError, error, data: results } = query;
 
-  return <Stack align="flex-start" gap="md">
-    <LoadingOverlay visible={isPending} />
-    {isError && (
-      <Text color="red">
-        {t("errorMessage", { error: error.message })}
-      </Text>
-    )}
-    {!isPending && !isError && (
-      <Stack>
-        {results.map((result) => <DefinedActivityResultWrapper result={result} />)}
-      </Stack>
-    )}
-  </Stack>;
+  return (
+    <Stack align="flex-start" gap="md">
+      <LoadingOverlay visible={isPending} />
+      {isError && (
+        <Text color="red">{t("errorMessage", { error: error.message })}</Text>
+      )}
+      {!isPending && !isError && (
+        <Stack>
+          {results.map((result) => (
+            <DefinedActivityResultWrapper result={result} />
+          ))}
+        </Stack>
+      )}
+    </Stack>
+  );
 };
