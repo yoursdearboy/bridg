15add395991e6ee70240ace4bd33e4c23b24eba6
yuurrraaaa <danilkin.ur.work@gmail.com>
2025-10-19 23:04:52 +0300

    add edit form

--- packages/ui/src/routes/spaces/$spaceId/subjects/$subjectId/activities/$obsId.edit.tsx
+++ packages/ui/src/routes/spaces/$spaceId/subjects/$subjectId/activities/$obsId.edit.tsx
@@ -1,8 +1,10 @@
-import { queryOptions } from "@tanstack/react-query";
+import { LoadingOverlay, Text } from "@mantine/core";
+import { queryOptions, useQuery } from "@tanstack/react-query";
 import { createFileRoute } from "@tanstack/react-router";
 import type { PerformedObservation } from "api-ts";
-import i18next from "i18next";
+import i18next, { t } from "i18next";
 import api from "@/api";
+import { ActivityFormWrapper } from "@/components/activity/ActivityForm";
 
 export const Route = createFileRoute(
   "/spaces/$spaceId/subjects/$subjectId/activities/$obsId/edit"
@@ -35,9 +37,39 @@ export const Route = createFileRoute(
 });
 
 function EditObservationComponent() {
+  const observation = Route.useLoaderData();
+  const { spaceId, subjectId } = Route.useParams();
+  const query = useQuery({
+    queryKey: [
+      "subjects",
+      subjectId,
+      "obsdervations",
+      observation.id,
+      "result",
+    ],
+    queryFn: () =>
+      api.subjects.indexSpacesSpaceIdSubjectsSubjectIdObservationObsIdResultGet(
+        {
+          spaceId,
+          subjectId,
+          obsId: observation.id,
+        }
+      ),
+  });
+  const { isPending, isError, error, data: results } = query;
   return (
-    <div>
-      Hello "/spaces/$spaceId/subjects/$subjectId/activities/$saId/edit"!
-    </div>
+    <>
+      <LoadingOverlay visible={isPending} />
+      {isError && (
+        <Text color="red">{t("errorMessage", { error: error.message })}</Text>
+      )}
+      {!isPending && !isError && (
+        <ActivityFormWrapper
+          spaceId={spaceId}
+          subjectId={subjectId}
+          results={results}
+        />
+      )}
+    </>
   );
 }
