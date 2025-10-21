6e8a51952c14907e375377e78709d13130623df2
Kirill Voronin <yoursdearboy@gmail.com>
2025-10-21 11:58:21 +0300

    Fixed new activity page query

--- packages/ui/src/routes/spaces/$spaceId/subjects/$subjectId/activities/new.tsx
+++ packages/ui/src/routes/spaces/$spaceId/subjects/$subjectId/activities/new.tsx
@@ -1,12 +1,7 @@
-import { Card, Group, LoadingOverlay, Stack, Text, Title } from "@mantine/core";
-import {
-  queryOptions,
-  useQuery,
-  useSuspenseQuery,
-} from "@tanstack/react-query";
+import { Card, Group, Stack, Title } from "@mantine/core";
+import { queryOptions, useSuspenseQuery } from "@tanstack/react-query";
 import { createFileRoute } from "@tanstack/react-router";
 import type { DefinedObservationResult, StudyActivity } from "api-ts";
-import { t } from "i18next";
 import api from "@/api";
 import { ActivityFormWrapper } from "@/components/activity/ActivityForm";
 import { defined2performedResult } from "@/components/activity/helpers/defined2performedResult";
@@ -26,16 +21,31 @@ export const Route = createFileRoute(
     };
   },
   beforeLoad: ({ params, search }) => ({
-    breadcrumb: ({ loaderData: activity }: { loaderData: StudyActivity }) =>
+    breadcrumb: ({
+      loaderData: { activity },
+    }: {
+      loaderData: { activity: StudyActivity };
+    }) =>
       activity.usedDefinedActivity.nameCode.displayName ||
       i18next.t("NewActivityRoute.breadcrumbDefault"),
     query: queryOptions({
       queryKey: ["subject", params.subjectId, "activity", "new"],
-      queryFn: () =>
-        api.spaceActivity.showSpacesSpaceIdActivitySaIdGet({
-          spaceId: params.spaceId,
-          saId: search.saId,
-        }),
+      queryFn: async () => {
+        const activity =
+          await api.spaceActivity.showSpacesSpaceIdActivitySaIdGet({
+            spaceId: params.spaceId,
+            saId: search.saId,
+          });
+        const results =
+          await api.spaceActivity.indexSpacesSpaceIdActivityObsIdResultGet({
+            spaceId: params.spaceId,
+            obsId: activity.id,
+          });
+        return {
+          activity,
+          results,
+        };
+      },
     }),
   }),
   loader: async ({ context: { query, queryClient } }) =>
@@ -44,17 +54,10 @@ export const Route = createFileRoute(
 
 function NewActivityComponent() {
   const { query } = Route.useRouteContext();
-  const { data: activity } = useSuspenseQuery(query);
+  const {
+    data: { activity, results },
+  } = useSuspenseQuery(query);
   const { spaceId, subjectId } = Route.useParams();
-  const resultsQuery = useQuery({
-    queryKey: ["subjects", subjectId, "activities", activity.id, "result"],
-    queryFn: () =>
-      api.spaceActivity.indexSpacesSpaceIdActivityObsIdResultGet({
-        spaceId,
-        obsId: activity.id,
-      }),
-  });
-  const { isPending, isError, error, data: results } = resultsQuery;
 
   return (
     <Stack gap="md">
@@ -65,17 +68,11 @@ function NewActivityComponent() {
       </Group>
 
       <Card>
-        <LoadingOverlay visible={isPending} />
-        {isError && (
-          <Text color="red">{t("errorMessage", { error: error.message })}</Text>
-        )}
-        {!isPending && !isError && (
-          <ActivityFormWrapperT
-            spaceId={spaceId}
-            subjectId={subjectId}
-            results={results}
-          />
-        )}
+        <ActivityFormWrapperT
+          spaceId={spaceId}
+          subjectId={subjectId}
+          results={results}
+        />
       </Card>
     </Stack>
   );
