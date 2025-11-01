edb9a878f3fb162aa22bbd46041b604302f99a3d
Kirill Voronin <yoursdearboy@gmail.com>
2025-11-01 17:28:18 +0300

    Refactored ConceptDescriptor module name

--- packages/bridg/bridg/core/__init__.py
+++ packages/bridg/bridg/core/__init__.py
@@ -1,7 +1,7 @@
 from datetime import date, datetime
 
 from .ad import PostalAddress, PostalAddressUse
-from .concept_descriptor import ConceptDescriptor, ConceptDescriptorService
+from .cd import ConceptDescriptor, ConceptDescriptorService
 from .pq import PhysicalQuantity
 from .tel import TelecommunicationAddress, TelecommunicationAddressUse
 from .url import URL, URLScheme
