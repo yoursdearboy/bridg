bd5e42c47074b30399d73594693839c40304cd71
Kirill Voronin <yoursdearboy@gmail.com>
2025-11-24 11:45:18 +0300

    Formatted

--- packages/ui/src/routes/spaces/$spaceId/subjects/$subjectId/index.tsx
+++ packages/ui/src/routes/spaces/$spaceId/subjects/$subjectId/index.tsx
@@ -44,7 +44,7 @@ function SubjectShowPage() {
           )}
         </Grid.Col>
         <Grid.Col span={{ base: 12, md: 6 }}>
-          <ActivityCard spaceId={spaceId} subjectId={subjectId}></ActivityCard>
+          <ActivityCard spaceId={spaceId} subjectId={subjectId} />
         </Grid.Col>
       </Grid>
     </Stack>
