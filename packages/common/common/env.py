21c0f8a50bb79514900ab9910013e537a8303d91
Kirill Voronin <yoursdearboy@gmail.com>
2025-12-16 23:00:36 +0300

    Fixed typo

--- packages/common/common/env.py
+++ packages/common/common/env.py
@@ -3,11 +3,11 @@ import os
 from dotenv import load_dotenv
 
 
-def get_env(prefix: str = "BRIGD"):
+def get_env(prefix: str = "BRIDG"):
     return os.environ.get(f"{prefix}_ENV", "development")
 
 
-def load_env(prefix: str = "BRIGD"):
+def load_env(prefix: str = "BRIDG"):
     env = get_env(prefix)
     path = f".env.{env}"
     load_dotenv()
