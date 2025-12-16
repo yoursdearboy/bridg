323713fb5efb0ebf343bffe596f3555e83f76fc4
Kirill Voronin <yoursdearboy@gmail.com>
2025-12-17 01:56:12 +0300

    Added e2e tests using playwright

---
+++ packages/ui/tests/datetime.spec.ts
@@ -0,0 +1,39 @@
+import { test, expect } from "@playwright/test";
+
+test.use({
+  timezoneId: "Arctic/Longyearbyen",
+});
+
+test("test", async ({ page }) => {
+  await page.goto("/spaces/ce946229-9746-46cd-8dd3-b27a2fbfd48a/subjects");
+  await page.getByRole("button", { name: "New patient" }).click();
+  await page.getByRole("textbox", { name: "Family name" }).click();
+  await page.getByRole("textbox", { name: "Family name" }).fill("Test");
+  await page.getByRole("button", { name: "Submit" }).click();
+  await page.getByText("Study sites").click();
+  await page.getByText("DGOI").click();
+  await page.getByText("StatusStatus dateStudy").click();
+  await page.getByRole("button", { name: "Submit" }).click();
+  await page.getByRole("button", { name: "Add" }).click();
+  await page.getByRole("menuitem", { name: "Laboratory" }).click();
+  await page.getByRole("menuitem", { name: "Immunophenotyping" }).click();
+  await page.getByRole("textbox", { name: "Status", exact: true }).click();
+  await page.getByRole("option", { name: "Complete", exact: true }).click();
+  await page.getByRole("textbox", { name: "Status date" }).click();
+  await page
+    .getByRole("button", { name: "1 December 2025", exact: true })
+    .click();
+  await page.locator("form").getByRole("button", { name: "Submit" }).click();
+  await page.getByRole("cell").nth(6).hover();
+  await page.getByRole("cell").nth(6).getByRole("link").click();
+  await expect(page.getByRole("textbox", { name: "Status date" })).toHaveValue(
+    "12/01/2025"
+  );
+  await page.getByRole("textbox", { name: "Status date" }).click();
+  await page.locator("form").getByRole("button", { name: "Submit" }).click();
+  await page.getByRole("cell").nth(6).hover();
+  await page.getByRole("cell").nth(6).getByRole("link").click();
+  await expect(page.getByRole("textbox", { name: "Status date" })).toHaveValue(
+    "12/01/2025"
+  );
+});
--- packages/ui/vitest.config.js
+++ packages/ui/vitest.config.js
@@ -5,6 +5,7 @@ export default defineConfig({
   test: {
     globals: true,
     environment: "jsdom",
+    exclude: ["tests"],
     setupFiles: "vitest.setup.ts",
   },
   resolve: {
