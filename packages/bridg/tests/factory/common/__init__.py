9677f1b88f0de904d2378cc9f6421af1b8e37722
Kirill Voronin <yoursdearboy@gmail.com>
2025-12-25 16:38:55 +0300

    Added factories for specimen

--- packages/bridg/tests/factory/common/__init__.py
+++ packages/bridg/tests/factory/common/__init__.py
@@ -1,4 +1,5 @@
 from .entity_name import EntityNameFactory
+from .material import MaterialFactory
 from .person import PersonFactory
 from .study import StudyFactory
 from .study_subject import StudySubjectFactory
--- packages/bridg/tests/factory/study/__init__.py
+++ packages/bridg/tests/factory/study/__init__.py
@@ -1,4 +1,5 @@
 from .performed_activity import PerformedActivityFactory
 from .performed_observation import PerformedObservationFactory
 from .performed_observation_result import PerformedObservationResultFactory
+from .performed_specimen_collection import PerformedSpecimenCollectionFactory
 from .study_site import StudySiteFactory
