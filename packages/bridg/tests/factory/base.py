fe8845fd22cbec7620c18571b105dd6993ed261b
Kirill Voronin <yoursdearboy@gmail.com>
2025-12-25 14:27:07 +0300

    Added TZDateTime support to SQLAlchemy BaseFactory

--- packages/bridg/tests/factory/base.py
+++ packages/bridg/tests/factory/base.py
@@ -1,8 +1,9 @@
-from typing import TypeVar
+from datetime import timezone
+from typing import Any, Callable, TypeVar
 
 from polyfactory.factories.sqlalchemy_factory import SQLAlchemyFactory
 
-from bridg.db import Base
+from bridg import Base, TZDateTime
 
 T = TypeVar("T", bound=Base)
 
@@ -12,3 +13,11 @@ class BaseFactory(SQLAlchemyFactory[T]):
     __set_relationships__ = True
     __set_association_proxy__ = True
     __check_model__ = True
+
+    @classmethod
+    def get_sqlalchemy_types(cls) -> dict[Any, Callable[[], Any]]:
+        types = super().get_sqlalchemy_types()
+        return {
+            **types,
+            TZDateTime: lambda: cls.__faker__.date_time_this_century(after_now=True, tzinfo=timezone.utc),
+        }
