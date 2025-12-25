6e3806c12e0fc9a564c7d4c2d4c2d8f07936f287
Kirill Voronin <yoursdearboy@gmail.com>
2025-12-25 14:13:53 +0300

    Setup database reset in tests

--- packages/api/conftest.py
+++ packages/api/conftest.py
@@ -1,5 +1,5 @@
 from common.env import load_env
 
-from tests.fixtures import session, snapshot_json  # noqa: F401
+from tests.fixtures import database, random, session, snapshot_json  # noqa: F401
 
 load_env()
--- packages/common/tests/fixtures.py
+++ packages/common/tests/fixtures.py
@@ -1,9 +1,13 @@
 import pytest
+from bridg.db import Base
+from polyfactory.factories.base import BaseFactory
 from sqlalchemy import create_engine
 from sqlalchemy.orm import Session
+from syrupy.assertion import SnapshotAssertion
+from syrupy.extensions.json import JSONSnapshotExtension
 
 from common.settings import load_settings
-from tests.factory.base import BaseFactory
+from tests.factory.base import BaseFactory as SQLAlchemyBaseFactory
 
 
 @pytest.fixture(scope="session", autouse=True)
@@ -12,9 +16,25 @@ def session():
     engine = create_engine(settings.SQLALCHEMY_DATABASE_URI)
     session = Session(engine)
 
-    BaseFactory.__session__ = session
+    SQLAlchemyBaseFactory.__session__ = session
 
     return session
+
+
+@pytest.fixture(scope="function", autouse=True)
+def database(session):
+    Base.metadata.create_all(session.bind)
+    yield
+    Base.metadata.drop_all(session.bind)
+
+
+@pytest.fixture(scope="function", autouse=True)
+def random():
+    BaseFactory.__random__.seed(42)
+    BaseFactory.__faker__.seed_instance(42)
+    BaseFactory.__faker__.unique.clear()
+    return BaseFactory.__random__
+
 
 @pytest.fixture
 def snapshot_json(snapshot) -> SnapshotAssertion:
