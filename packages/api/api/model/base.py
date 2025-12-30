e4b0bccf0353ad5e7ba00dd34e7a9befc3eaeedf
Kirill Voronin <yoursdearboy@gmail.com>
2025-12-30 12:39:36 +0300

    Cleaned up __init__.py files

--- packages/api/api/model/base.py
+++ packages/api/api/model/base.py
@@ -6,16 +6,16 @@ from pydantic import ConfigDict, PrivateAttr
 T = TypeVar("T")
 
 
-def omit(keys, x: dict) -> dict:
+def _omit(keys, x: dict) -> dict:
     return {k: v for k, v in x.items() if k not in keys}
 
 
-def dump(x: Any, context: Any = None) -> Any:
+def _dump(x: Any, context: Any = None) -> Any:
     match x:
         case dict():
-            return {k: dump(v, context=context) for k, v in x.items()}
+            return {k: _dump(v, context=context) for k, v in x.items()}
         case list():
-            return [dump(v, context=context) for v in x]
+            return [_dump(v, context=context) for v in x]
         case BaseModel():
             return x.model_dump_sa(context=context)
         case _:
@@ -27,13 +27,13 @@ class BaseModel(PydanticBaseModel, Generic[T]):
     _sa: type[T] = PrivateAttr()
 
     def model_dump_sa(self, exclude: Set = set(), context: Any = None) -> T:
-        data = dump(dict(self), context=context)
-        data = omit(exclude, data)
+        data = _dump(dict(self), context=context)
+        data = _omit(exclude, data)
         return self._sa(**data)
 
     def model_update_sa(self, obj: T, exclude=set(), context: Any | None = None) -> T:
-        data = dump(dict(self), context=context)
-        data = omit(exclude, data)
+        data = _dump(dict(self), context=context)
+        data = _omit(exclude, data)
         for key, value in data.items():
             setattr(obj, key, value)
         return obj
