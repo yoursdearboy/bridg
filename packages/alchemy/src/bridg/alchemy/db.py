ca2e5aa901aeeb72460aeaa8948f03843569102d
Kirill Voronin <yoursdearboy@gmail.com>
2026-01-05 15:03:42 +0300

    Added UniqueIdentifierString to Base

--- packages/alchemy/src/bridg/alchemy/db.py
+++ packages/alchemy/src/bridg/alchemy/db.py
@@ -1,11 +1,14 @@
 from datetime import datetime
 
+from sqlalchemy import String
 from sqlalchemy.orm import DeclarativeBase
 
+from .datatype.uid import UniqueIdentifierString
 from .tz_date_time import TZDateTime
 
 
 class Base(DeclarativeBase):
     type_annotation_map = {
         datetime: TZDateTime,
+        UniqueIdentifierString: String,
     }
