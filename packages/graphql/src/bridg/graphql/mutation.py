03e379ee732109af74f9eb09346b029a5b2e5358
Kirill Voronin <yoursdearboy@gmail.com>
2026-02-09 02:16:08 +0300

    Refactoring

--- packages/graphql/src/bridg/graphql/mutation.py
+++ packages/graphql/src/bridg/graphql/mutation.py
@@ -1,4 +1,7 @@
-from typing import Type, get_origin
+import re
+import typing
+from dataclasses import is_dataclass
+from typing import Annotated, Any, List, Type, get_args, get_origin
 
 import strawberry
 from sqlalchemy import inspect
@@ -11,57 +14,77 @@ from .context import Context
 
 
 def get_concrete_class[T: bridg.alchemy.Base](input, class_: Type[T]) -> Type[T]:
-    ins = inspect(class_)
-    if (polymorphic_on := ins.polymorphic_on) is not None:
+    insp = inspect(class_)
+    if (polymorphic_on := insp.polymorphic_on) is not None:
         if polymorphic_value := getattr(input, polymorphic_on.name, None):
-            return ins.polymorphic_map[polymorphic_value].class_
+            return insp.polymorphic_map[polymorphic_value].class_
     return class_
 
 
-def _convert[T: bridg.alchemy.Base](
-    input, class_: Type[T], terminology: bridg.alchemy.TerminologyService | None = None
-) -> T:
+_maybe_re = re.compile(r"^(?:strawberry\.)?Maybe\[(.+)\]$")
+
+
+def _annotation_is_maybe(annotation: Any) -> bool:
+    if isinstance(annotation, str):
+        # Ideally we would try to evaluate the annotation, but the args inside
+        # may still not be available, as the module is still being constructed.
+        # Checking for the pattern should be good enough for now.
+        return _maybe_re.match(annotation) is not None
+
+    orig = typing.get_origin(annotation)
+    if orig is Annotated:
+        return _annotation_is_maybe(typing.get_args(annotation)[0])
+    return orig is strawberry.Maybe
+
+
+def _convert[T](input, class_: Type[T], terminology: bridg.alchemy.TerminologyService | None = None) -> T:
+    if is_dataclass(class_):
+        return class_(**{k: getattr(input, k) for k in class_.__dataclass_fields__.keys()})
+
+    if get_origin(class_) is list:
+        (arg,) = get_args(class_)
+        return [_convert(x, arg, terminology=terminology) for x in input]  # type: ignore
+
     if class_ == bridg.alchemy.ConceptDescriptor:
         if terminology is None:
             raise RuntimeError("TerminologyService required")
         return terminology.get_or_create(input.code, input.code_system, input.display_name)  # type: ignore
 
-    class_ = get_concrete_class(input, class_)
-    ins = inspect(class_)
-    # hints = get_type_hints(class_, localns=bridg.alchemy.__dict__)
-    output = class_()
-    for key, field in input.__dataclass_fields__.items():
-        value = getattr(input, key, None)
+    if issubclass(class_, bridg.alchemy.Base):
+        class_ = get_concrete_class(input, class_)
+        insp = inspect(class_)
+        output = class_()
+        for key, field in input.__dataclass_fields__.items():
+            value = getattr(input, key, None)
 
-        # Handle maybe
-        if get_origin(field.type) == strawberry.Maybe or (
-            isinstance(field.type, str) and (field.type.startswith("strawberry.Maybe"))
-        ):
-            if value is None:
-                continue
-            value = value.value
+            if _annotation_is_maybe(field.type):
+                if value is None:
+                    continue
+                value = value.value
 
-        if value is not None:
-            if attr := ins.attrs.get(key):
+            if value is not None:
+                attr = insp.attrs.get(key)
+
+                if attr is None:
+                    raise RuntimeError(f"There's no attr {key} in the model {class_.__name__}")
+
                 if isinstance(attr, Relationship):
                     attr_class_ = attr.entity.class_
                     if attr.uselist:
-                        value = [_convert(v, attr_class_, terminology=terminology) for v in value]
-                    else:
-                        value = _convert(value, attr_class_, terminology=terminology)
-                elif isinstance(attr, Composite):
+                        attr_class_ = List[attr_class_]
+                    value = _convert(value, attr_class_, terminology=terminology)
+
+                if isinstance(attr, Composite):
                     attr_class_ = attr.composite_class
-                    value = attr_class_(**{k: getattr(value, k) for k in attr_class_.__dataclass_fields__.keys()})
-                else:
-                    # raise RuntimeError(f"Unknown attr {key} type in the model {class_.__name__}")
-                    # it must be primitive, so just don't convert
-                    pass
-            else:
-                raise RuntimeError(f"There's no attr {key} in the model {class_.__name__}")
+                    assert isinstance(attr_class_, type)
+                    value = _convert(value, attr_class_, terminology=terminology)
 
-        setattr(output, key, value)
+                # otherwise it must be primitive, so just don't convert
 
-    return output
+            setattr(output, key, value)
+        return output
+
+    raise RuntimeError(f"Can't comvert to {class_.__name__}")
 
 
 @strawberry.type
