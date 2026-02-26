82f7d5ce86333fd78069063476c00247a99106a7
Kirill Voronin <yoursdearboy@gmail.com>
2026-02-26 03:28:55 +0300

    graphql: camel case scalar keys

--- packages/graphql/src/bridg/graphql/scalar.py
+++ packages/graphql/src/bridg/graphql/scalar.py
@@ -1,18 +1,23 @@
 from strawberry.types.scalar import ScalarDefinition, scalar
+from strawberry.utils.str_converters import to_camel_case
 
 from .datatype import ConceptDescriptor, InstanceIdentifier
 
 
 def _serialize_dataclass(cl):
+    keys = cl.__dataclass_fields__.keys()
+
     def serialize(x):
-        return {k: getattr(x, k) for k in cl.__dataclass_fields__.keys()}
+        return {to_camel_case(k): getattr(x, k) for k in keys}
 
     return serialize
 
 
 def _parse_dataclass(cl):
+    keys = cl.__dataclass_fields__.keys()
+
     def parse(x):
-        return cl(**{k: x.get(k) for k in cl.__dataclass_fields__.keys()})
+        return cl(**{k: x.get(to_camel_case(k)) for k in keys})
 
     return parse
 
