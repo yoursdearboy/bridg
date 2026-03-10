685fb2b48ba1e498280f4ebd71ffc31df98ac5f3
Kirill Voronin <yoursdearboy@gmail.com>
2026-03-11 02:47:35 +0300

    feat(graphql): add involving_performed_activity support to Subject

--- packages/graphql/src/bridg/graphql/schema/common/subject.py
+++ packages/graphql/src/bridg/graphql/schema/common/subject.py
@@ -1,9 +1,10 @@
 from __future__ import annotations
 
-from typing import Optional
+from typing import List, Optional
 
 import strawberry
 
+from ..study import PerformedActivity
 from .biologic_entity import BiologicEntity, BiologicEntityInput
 
 
@@ -11,6 +12,7 @@ from .biologic_entity import BiologicEntity, BiologicEntityInput
 class Subject:
     id: strawberry.ID
     performing_biologic_entity: Optional[BiologicEntity]
+    involving_performed_activity: List[PerformedActivity]
 
 
 @strawberry.input
