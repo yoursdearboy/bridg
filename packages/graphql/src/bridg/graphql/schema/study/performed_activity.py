73a81f09f1429125ecb7b819a6e489c0cb141f9a
Kirill Voronin <yoursdearboy@gmail.com>
2026-03-11 02:13:38 +0300

    feat(graphql): basic PerformedActivity support

---
+++ packages/graphql/src/bridg/graphql/schema/study/performed_activity.py
@@ -0,0 +1,31 @@
+from datetime import datetime
+from typing import Optional
+
+import strawberry
+
+import bridg.alchemy
+
+from ..datatype import ConceptDescriptor
+
+
+@strawberry.interface
+class PerformedActivityInterface:
+    id: strawberry.ID
+    repetition_number: Optional[int]
+    name_code_modified_text: Optional[str]
+    # date_range: Optional[IntervalPointInTime]
+    negation_indicator: Optional[bool]
+    negation_reason: Optional[ConceptDescriptor]
+    status_code: Optional[ConceptDescriptor]
+    status_date: Optional[datetime]
+    # containing_epoch: Optional[Epoch]
+    # executing_study_protocol_version: Optional[StudyProtocolVersion]
+    # instantiated_defined_activity: Optional[DefinedActivity]
+    # involved_subject: Optional[StudySubject]
+
+
+@strawberry.type
+class PerformedActivity(PerformedActivityInterface):
+    @staticmethod
+    def is_type_of(obj, _) -> bool:
+        return isinstance(obj, bridg.alchemy.PerformedActivity)
---
+++ packages/graphql/tests/study/test_performed_activity.py
@@ -0,0 +1,77 @@
+from syrupy.matchers import path_type
+
+from bridg.alchemy.factory import (
+    PerformedActivityFactory,
+    PersonFactory,
+    StudyProtocolVersionFactory,
+    StudySubjectFactory,
+)
+from bridg.graphql.context import Context
+from bridg.graphql.schema import schema
+
+from ..utils import process_input
+
+
+def test_performed_activity_query(context: Context, snapshot_json):
+    space = StudyProtocolVersionFactory.create_sync()
+    sspvr = space.executing_study_site_protocol_version_relationship[0]
+    ss = StudySubjectFactory.create_sync(
+        performing_biologic_entity=PersonFactory.build(),
+        performing_organization=None,
+        assigned_study_site_protocol_version_relationship=[sspvr],
+    )
+    act = PerformedActivityFactory.create_sync(
+        executing_study_protocol_version=space,
+        involved_subject=ss,
+    )
+
+    query = """
+        query($id: ID!) {
+            PerformedActivity(id: $id) {
+                id
+                repetitionNumber
+                nameCodeModifiedText
+                negationReason
+                negationIndicator
+                statusCode
+                statusDate
+            }
+        }
+    """
+
+    result = schema.execute_sync(query, process_input(dict(id=act.id)), context_value=context)
+    assert result.errors is None
+    assert result.data == snapshot_json(matcher=path_type({r".*id$": (str,)}, regex=True))
+
+
+def test_performed_activity_list_query(context: Context, snapshot_json):
+    space = StudyProtocolVersionFactory.create_sync()
+    sspvr = space.executing_study_site_protocol_version_relationship[0]
+    ss = StudySubjectFactory.create_sync(
+        performing_biologic_entity=PersonFactory.build(),
+        performing_organization=None,
+        assigned_study_site_protocol_version_relationship=[sspvr],
+    )
+    PerformedActivityFactory.create_batch_sync(
+        2,
+        executing_study_protocol_version=space,
+        involved_subject=ss,
+    )
+
+    query = """
+        query {
+            PerformedActivityList {
+                id
+                repetitionNumber
+                nameCodeModifiedText
+                negationReason
+                negationIndicator
+                statusCode
+                statusDate
+            }
+        }
+    """
+
+    result = schema.execute_sync(query, context_value=context)
+    assert result.errors is None
+    assert result.data == snapshot_json(matcher=path_type({r".*id$": (str,)}, regex=True))
