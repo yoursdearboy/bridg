788ee8cc027ec1c0a039e402349248d8569bde6e
Kirill Voronin <yoursdearboy@gmail.com>
2026-03-12 18:45:00 +0300

    Shrinked the project

--- dev/test.sh
+++ dev/test.sh
@@ -2,5 +2,4 @@
 export BRIDG_ENV=testing
 dropdb -h localhost -U postgres bridg-testing
 createdb -h localhost -U postgres bridg-testing
-python -m dev.migrate
 pytest ${@}
--- packages/api/tests/factory/common/id.py
+++
@@ -1,9 +0,0 @@
-from bridg.api.model import ID
-
-from ..base import BaseFactory
-from ..datatype import ConceptDescriptorFactory, InstanceIdentifierFactory
-
-
-class IDFactory(BaseFactory[ID]):
-    identifier: InstanceIdentifierFactory
-    identifier_type_code = ConceptDescriptorFactory
