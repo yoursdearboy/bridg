24a0dc1373a15056e858deea83e320981dcf7a59
Kirill Voronin <yoursdearboy@gmail.com>
2026-03-15 15:00:16 +0300

    tests(graphql): fix PerformedActivityInputFactory

--- packages/graphql/tests/factory/study/performed_activity.py
+++ packages/graphql/tests/factory/study/performed_activity.py
@@ -1,3 +1,5 @@
+from datetime import timezone
+
 from polyfactory import Ignore, Use
 
 from bridg.graphql.schema import PerformedActivityInput
@@ -18,7 +20,9 @@ class PerformedActivityInputFactory[T: PerformedActivityInput](ActivityInputFact
     negation_indicator = False
     negation_reason = None
     # status_code
-    status_date = Use(make_some(lambda: BaseFactory.__faker__.date_this_century(after_today=True)))
+    status_date = Use(
+        make_some(lambda: BaseFactory.__faker__.date_time_this_century(after_now=True, tzinfo=timezone.utc))
+    )
     containing_epoch_id = None
     # TODO: add these properties
     # executing_study_protocol_version: Optional[StudyProtocolVersion]
