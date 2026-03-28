8199f1b5986c806b04dd9e0990e9e81285934b31
Kirill Voronin <yoursdearboy@gmail.com>
2026-03-28 11:19:01 +0300

    tests(graphql): add test_study_protocol_version_list

--- packages/graphql/tests/protocol/test_study_protocol_version.py
+++ packages/graphql/tests/protocol/test_study_protocol_version.py
@@ -1,11 +1,51 @@
 from syrupy.matchers import path_type
 
 from bridg.alchemy import Organization, OrganizationName
-from bridg.alchemy.factory import StudyProtocolVersionFactory, StudySiteFactory
+from bridg.alchemy.factory import OrganizationFactory, StudyProtocolVersionFactory, StudySiteFactory
 from bridg.graphql.context import Context
 from bridg.graphql.schema import schema
 
 from ..utils import process_input
+
+
+def test_study_protocol_version_list(context: Context, snapshot_json):
+    org = OrganizationFactory.create_sync(name=[OrganizationName(value="Lead Org LTD")])
+    StudyProtocolVersionFactory.create_sync(
+        acronym="TEST-1",
+        executing_study_site=[
+            StudySiteFactory.build(lead=True, performing_organization=org),
+            StudySiteFactory.build(),
+            StudySiteFactory.build(),
+        ],
+    )
+    StudyProtocolVersionFactory.create_sync(
+        acronym="TEST-2",
+        executing_study_site=[
+            StudySiteFactory.build(lead=True, performing_organization=org),
+            StudySiteFactory.build(),
+        ],
+    )
+    query = """
+        query {
+            StudyProtocolVersionList {
+                id
+                acronym
+                executingStudySite {
+                    id
+                    lead
+                    performingOrganization {
+                        id
+                        primaryName {
+                            value
+                        }
+                    }
+                }
+            }
+        }
+    """
+    result = schema.execute_sync(query, context_value=context)
+    assert result.errors is None
+    assert result.data == snapshot_json(matcher=path_type({r".*id$": (str,)}, regex=True))
 
 
 def test_study_protocol_version(context: Context, snapshot_json):
