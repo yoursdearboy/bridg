fb26aaaa36187b0f2d00c09ed38b113c53f1866e
Kirill Voronin <yoursdearboy@gmail.com>
2026-04-03 18:44:31 +0300

    tests(graphql): fixed PerformedActivityInputFactory

--- packages/graphql/tests/factory/study/performed_activity.py
+++ packages/graphql/tests/factory/study/performed_activity.py
@@ -1,12 +1,13 @@
 from datetime import timezone
 
 from polyfactory import Ignore, Use
+from strawberry import Some
 
 from bridg.graphql.schema import PerformedActivityInput
 
 from ..base import BaseFactory
 from ..common import ActivityInputFactory
-from ..datatype import IntervalPointInTimeFactory
+from ..datatype import ConceptDescriptorFactory, IntervalPointInTimeFactory
 from ..maybe import make_some
 
 
@@ -15,17 +16,17 @@ class PerformedActivityInputFactory[T: PerformedActivityInput](ActivityInputFact
 
     id = None
     type = Ignore()
-    repetition_number = 1
+    repetition_number = Some(1)
     name_code_modified_text = None
-    date_range = IntervalPointInTimeFactory
-    negation_indicator = False
+    date_range = Use(make_some(IntervalPointInTimeFactory.build))
+    negation_indicator = Some(False)
     negation_reason = None
-    # status_code
+    status_code = Use(make_some(ConceptDescriptorFactory.build))
     status_date = Use(
         make_some(lambda: BaseFactory.__faker__.date_time_this_century(after_now=True, tzinfo=timezone.utc))
     )
     containing_epoch_id = None
     # TODO: add these properties
     # executing_study_protocol_version: Optional[StudyProtocolVersion]
-    # instantiated_defined_activity: Optional[DefinedActivity]
+    instantiated_defined_activity_id = Ignore()
     involved_subject_id = Ignore()
