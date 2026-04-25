86bb0cb3653b03f2c7bc2c66fc33248452624878
Kirill Voronin <yoursdearboy@gmail.com>
2026-04-26 01:20:23 +0300

    tests(api): fix session setup for app in tests

--- packages/api/conftest.py
+++ packages/api/conftest.py
@@ -1,8 +1,11 @@
 import pytest
+from sqlalchemy.orm import Session
 from syrupy.assertion import SnapshotAssertion
 from syrupy.extensions.json import JSONSnapshotExtension
 
-from bridg.alchemy.test.fixture import database, random, session  # noqa: F401
+from bridg.alchemy.test.fixture import *  # noqa: F403  # pyright: ignore[reportWildcardImportFromLibrary]
+from bridg.api.db import get_db
+from bridg.api.main import app
 from bridg.common.env import load_env
 
 load_env()
@@ -11,3 +14,8 @@ load_env()
 @pytest.fixture
 def snapshot_json(snapshot) -> SnapshotAssertion:
     return snapshot.with_defaults(extension_class=JSONSnapshotExtension)
+
+
+@pytest.fixture(autouse=True)
+def app_db(session: Session):
+    app.dependency_overrides[get_db] = lambda: session
