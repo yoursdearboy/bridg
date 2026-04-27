71ab31de662b97038d372e6712e65e17ff35ced3
Kirill Voronin <yoursdearboy@gmail.com>
2026-04-27 18:03:22 +0300

    feat(graphql): add filter to PlaceList query

--- packages/graphql/src/bridg/graphql/schema/common/place.py
+++ packages/graphql/src/bridg/graphql/schema/common/place.py
@@ -34,10 +34,17 @@ class PlaceName:
     value: Optional[str]
 
 
+@strawberry.input
+class PlaceFilter:
+    type_code: Optional[ConceptDescriptor] = None
+
+
 @strawberry.type
 class PlaceQuery:
     @strawberry.field(name="PlaceList")
-    def place_list(self, *, info: strawberry.Info[Context]) -> List[Place]:
+    def place_list(self, filter: Optional[PlaceFilter] = None, *, info: strawberry.Info[Context]) -> List[Place]:
         session = info.context.session
         query = session.query(bridg.alchemy.Place)
+        if filter and (cd := filter.type_code):
+            query = query.filter(bridg.alchemy.Place.type_code.has(code=cd.code, code_system=cd.code_system))
         return query.all()  # type: ignore
